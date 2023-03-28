a57a025f656b3947dcef37a24839e1f1685b85b5
Maurya2811 <113296853+Maurya2811@users.noreply.github.com>
2023-03-29 02:22:19 +0530

    Time: 109 ms (99.06%), Space: 57.1 MB (96.07%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.cpp
+++ 0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -1,13 +1,23 @@
 class Solution {
 public:
     bool containsDuplicate(vector<int>& nums) {
-        // Using Hash Map  
-        unordered_map<int ,int> hash;
-        for(int i=0;i<nums.size();i++){
-            if(hash[nums[i]]>0)
-                return true;
-           hash[nums[i]]++;
+        // Using Hash Map or WE can use Set contain to store elements ans check size
+        
+       //  unordered_map<int ,int> hash;
+       //  for(int i=0;i<nums.size();i++){
+       //      if(hash[nums[i]]>0)
+       //          return true;
+       //     hash[nums[i]]++;
+       // }
+       //  return false;
+        
+        // Using Sorting
+         sort(nums.begin(),nums.end());
+        for(int i=1;i<nums.size();i++){
+           if(nums[i]==nums[i-1])
+               return true;
        }
         return false;
+        
     }
 };
