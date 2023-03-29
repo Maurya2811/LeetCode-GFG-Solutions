54aa263a78089a1a60beabac9c9823d9dea9ec48
Maurya2811 <113296853+Maurya2811@users.noreply.github.com>
2023-03-29 11:01:29 +0530

    Time: 3 ms (80.63%), Space: 11.1 MB (44.97%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
@@ -33,13 +33,14 @@ int findPivot(vector<int>& nums)
         int mid;
         // if(nums.size()>1 && nums[0]>nums[1])
         // return 0;
-         if(s==e)
-            return s;
+         
         
         while(s<=e)
         {  
+            if(s==e)
+            return s;
             mid=s+(e-s)/2;
-            if(mid+1<nums.size() && nums[mid]>nums[mid+1]  )
+            if( nums[mid]>nums[mid+1]  )
             return mid;
             else if(mid-1 >0 && nums[mid-1]>nums[mid]  )
             return mid-1;
