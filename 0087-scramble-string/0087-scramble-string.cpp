c911a9cd93f33f37500a7455d51c8ba73a1f65e6
Maurya2811 <113296853+Maurya2811@users.noreply.github.com>
2023-03-31 03:44:14 +0530

    Time: 168 ms (43.24%), Space: 37.4 MB (27.87%) - LeetHub

--- 0087-scramble-string/0087-scramble-string.cpp
+++ 0087-scramble-string/0087-scramble-string.cpp
@@ -1,39 +1,54 @@
+//Appoach-1 (Recursion + Memo)
 class Solution {
 public:
-    unordered_map<string,bool>map;
-    bool solve(string s1, string s2){
-        if(s1==s2)
+    unordered_map<string, bool> mp;
+    bool solve(string s1, string s2) {
+
+        if(s1 == s2) //both string empty covered here too
             return true;
-        if(s2.length()!=s1.length())
+        if(s1.length() != s2.length())
             return false;
-        string key = s1 +" "+s2; 
-        if(map.find(key)!=map.end())
-            return map[key];
-        bool result =false;
-        int n=s1.length();
-        for(int i=1;i<n;i++){
-            
-            bool swapped = solve(s1.substr(0,i),s2.substr(n-i,i)) 
-                              && 
-                           solve(s1.substr(i,n-i),s2.substr(0,n-i));
-            if(swapped==true){
-                result=swapped;
+
+        string key = s1 + "_" + s2;
+        if(mp.find(key) != mp.end())
+            return mp[key];
+
+        bool result = false;
+        int n = s1.length();
+        for(int i = 1; i<n; i++) {
+            /*
+            Example : "great"  "eatgr"
+            if swapped at i = 2
+            we compare s1.substr(0, i) i.e "gr"  with s2.substr(n-i, i) i.e. "gr"
+            && s1.substr(i, n-i) i.e "eat"  with s2.substr(0, n-i) i.e. "eat"
+            */
+
+            bool swapped = solve(s1.substr(0, i), s2.substr(n-i, i)) &&
+                           solve(s1.substr(i, n-i), s2.substr(0, n-i));
+            if(swapped) { //if we find they are scrambled, we don't need to check further
+                result = true;
                 break;
             }
-            
-            bool not_swapped = solve(s1.substr(0,i),s2.substr(0,i)) 
-                              && 
-                           solve(s1.substr(i,n-i),s2.substr(i,n-i));
-            
-            if(not_swapped==true){
-                result=not_swapped;
-               break;
+            /*
+            Example : "great"  "great"
+            if not swapped at i = 2
+            we compare s1.substr(0, i) i.e "gr"  with s2.substr(0, i) i.e. "gr"
+            && s1.substr(i, n-i) i.e "eat"  with s2.substr(i, n-i) i.e. "eat"
+            */
+
+            bool not_swapped = solve(s1.substr(0, i), s2.substr(0, i)) &&
+                               solve(s1.substr(i, n-i), s2.substr(i, n-i));
+            if(not_swapped) { //if we find they are scrambled, we don't need to check further
+                result = true;
+                break;
             }
+
         }
-        return map[key]=result;
+
+        return mp[key] = result;
     }
-    bool isScramble(string s1, string s2) {
-         map.clear();
-        return solve(s1,s2);
+    bool isScramble(string &s1, string &s2) {
+        mp.clear();
+        return solve(s1, s2);
     }
 };
