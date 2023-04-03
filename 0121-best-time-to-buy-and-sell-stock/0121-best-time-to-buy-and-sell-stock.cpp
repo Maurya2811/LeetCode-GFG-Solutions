ecc94e718247745b9293d6391b634bd265ce9102
Maurya2811 <113296853+Maurya2811@users.noreply.github.com>
2023-04-03 18:53:40 +0530

    Time: 144 ms (36.80%), Space: 101.2 MB (5.40%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
@@ -1,19 +1,54 @@
+//  Using Iteration  
+/*
 class Solution {
 public:
+    
+   
     int maxProfit(vector<int>& prices) {
-         
+       
         // Kadane's Algo
         int maxProfit=0;
-        int diff,min=prices[0];
-        int size=prices.size(),i,j;
+        int diff,mini=prices[0];
+        int size=prices.size(),i;
         for(i=0;i<size;i++)
         { 
-            if(prices[i]<min)  
-             min=prices[i];
+              
+             mini= min(prices[i],mini);
 
-           diff=prices[i]-min;
+           diff=prices[i]-mini;
                maxProfit=max(diff,maxProfit);
         }
         return maxProfit;
+    }
+};
+
+*/
+
+// Using Recursion
+class Solution {
+public:
+    
+    void solve (vector<int>& prices, int &maxProfit , int &mini,int i){
+        // Base case
+        if(i==prices.size())
+            return ;
+        // Solving 1 case
+         
+         mini= min(prices[i],mini);
+         int  diff=prices[i]-mini;
+               maxProfit=max(diff,maxProfit);
+        solve(prices,maxProfit , mini,i+1);
+    }
+    
+    
+    int maxProfit(vector<int>& prices) {
+       
+        // Using Recursion
+         int maxProfit=0;
+        int mini=prices[0];
+        solve (prices,maxProfit , mini,0);
+        return maxProfit;
+        
+        
     }
 };
