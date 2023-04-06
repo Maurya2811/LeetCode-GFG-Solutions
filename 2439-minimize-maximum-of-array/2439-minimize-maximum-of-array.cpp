84109cc5ea75175efda9783ac0ab47d0f0543619
Maurya2811 <113296853+Maurya2811@users.noreply.github.com>
2023-04-06 13:15:36 +0530

    Time: 281 ms (17.03%), Space: 150.1 MB (18.38%) - LeetHub

--- 2439-minimize-maximum-of-array/2439-minimize-maximum-of-array.cpp
+++ 2439-minimize-maximum-of-array/2439-minimize-maximum-of-array.cpp
@@ -1,46 +1,37 @@
 class Solution {
 public:
     
-    bool isValid(vector<int> &nums, int mid_max, int n) {
-        
-        vector<long long> arr(begin(nums), end(nums));
-        
-        for(int i = 0; i<n-1; i++) {
-            
-            if(arr[i] > mid_max)
+    bool isValid(vector<int>& nums,int mid,int &n){
+        vector< long long > arr (nums.begin(),nums.end());
+     
+        for(int i=0;i<n-1;i++){
+            if(arr[i]>mid)
                 return false;
-            
-            long long buffer = mid_max - arr[i];
-            
-            arr[i+1] = arr[i+1] - buffer;   
-            
+           long long buffer=mid-arr[i];
+            arr[i+1]=arr[i+1]-buffer;  
         }
-        
-        return arr[n-1] <= mid_max;
-        
+        // if(arr[n-1]>mid)
+        //     return false; 
+        // return true;
+        return arr[n-1]<=mid;
     }
     
     int minimizeArrayValue(vector<int>& nums) {
-        int n = nums.size();
+        int size=nums.size();
+        int maxLeft=nums[0]; // since Number at 1st position cant be minimized
+        int maxRight = *max_element(nums.begin(),nums.end());
         
-        int maxL = 0;
-        int maxR = *max_element(begin(nums), end(nums));
-        
-        int result;
-        
-        while(maxL <= maxR) {
-            
-            int mid_max = maxL + (maxR-maxL)/2;
-            
-            if(isValid(nums, mid_max, n)) {
-                result = mid_max;
-                maxR   = mid_max-1;
-            } else {
-                maxL = mid_max+1;
+        int result =0;
+        while(maxLeft<=maxRight){
+            int mid = maxLeft + (maxRight-maxLeft)/2;
+            if(isValid(nums,mid,size)){
+                result = mid;
+                maxRight=mid-1;
             }
-            
+            else{
+                maxLeft=mid+1;
+            }
         }
-        
         return result;
     }
 };
