5823f8e7ef45b2c23cb9c030ff0927ce046532d8
Maurya2811 <113296853+Maurya2811@users.noreply.github.com>
2023-04-08 00:36:04 +0530

    Time: 4 ms (63.68%), Space: 11.7 MB (78.06%) - LeetHub

---
+++ 0022-generate-parentheses/0022-generate-parentheses.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+       vector<string> ans;
+    void solve(string &temp,int remaining_Open,int remaining_Close){
+        if(remaining_Open <=0 && remaining_Close<=0){
+            ans.push_back(temp);
+            return;
+        }
+        
+        // Case 1 When remaining Open brackets is greater than zero 
+        if(remaining_Open>0){
+              temp.push_back('(');
+            solve(temp,remaining_Open-1,remaining_Close);
+             temp.pop_back();
+        }
+         // Case 2 When remaining close brackets is more than remaining open brackets 
+          if(remaining_Close > remaining_Open ){
+              temp.push_back(')');
+            solve(temp,remaining_Open,remaining_Close-1);
+             temp.pop_back();
+        }
+    }
+    vector<string> generateParenthesis(int n) {
+     
+        string temp="";
+        int remaining_Open =n;
+        int remaining_Close =n;
+        solve(temp,remaining_Open,remaining_Close);
+        return ans;
+    }
+};
