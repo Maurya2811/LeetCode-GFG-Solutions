43b09bb4d1ff3e0718d403d4196fb0c10c7771ea
Maurya2811 <113296853+Maurya2811@users.noreply.github.com>
2023-04-07 10:49:26 +0530

    Time: 14 ms (61.94%), Space: 9.5 MB (77.45%) - LeetHub

--- 1254-number-of-closed-islands/1254-number-of-closed-islands.cpp
+++ 1254-number-of-closed-islands/1254-number-of-closed-islands.cpp
@@ -2,19 +2,20 @@ class Solution {
 public:
     int row , col;
     bool dfs(vector<vector<int>>& grid,int i,int j){
-        if(i>=row || i<0 || j>=col || j<0){
-            return false;
+        if(i>=row || i<0 || j>=col || j<0){ // Out of bound checking
+            return false;    // Not closed
         }
-         if(grid[i][j]==1)
-            return true;
+         if(grid[i][j]==1)  // water
+            return true;  // Closed from this side
         
-        grid[i][j]=1;
+        grid[i][j]=1;  // mark visited 
+        
         bool left_side =dfs(grid,i,j-1); // left
         bool right_side =dfs(grid,i,j+1);  // right
         bool up_side =dfs(grid,i-1,j);  // up
         bool down_side= dfs(grid,i+1,j);  // down
         
-        return (left_side && right_side && up_side && down_side);
+        return (left_side && right_side && up_side && down_side);   
     }    
     
     int closedIsland(vector<vector<int>>& grid) {
@@ -23,7 +24,7 @@ public:
         int count =0;
         for(int i=0;i<row;i++){
             for(int j=0;j<col;j++){
-                if(grid[i][j]==0)
+                if(grid[i][j]==0)    // Land
                 if(dfs(grid,i,j)){
                     count++;
                 }
