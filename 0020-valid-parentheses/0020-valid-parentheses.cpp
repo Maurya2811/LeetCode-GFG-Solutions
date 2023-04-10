33c6ea16284b33b99023b3149fa7fb546c659d91
Maurya2811 <113296853+Maurya2811@users.noreply.github.com>
2023-04-11 00:55:12 +0530

    Time: 4 ms (39.51%), Space: 6.4 MB (12.09%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.cpp
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -1,11 +1,19 @@
 // 2 Approaches 
 class Solution {
 public:
-    bool isValid(string s) {
-        // Approach 1 Using Stack
+    
+      // 1 Approach
+    
+   bool isValid(string s) {
+      /*  
+       // Approach 1 Using Stack
           stack<char> st;
         for(auto &ch: s){
             if(st.empty() || ch=='(' || ch=='{' || ch=='['){
+                // We are checking st.empty() because 1st bracket may me closed bracket
+                // we if we are checking for only Open brackets and pushing it
+                // if close bracket is not pushed then st.pop will make stack empty
+                // and the program will return it True which gives wrong ans;
                 st.push(ch);
             }
             else if(ch==')' && st.top()!='(')
@@ -19,5 +27,23 @@ public:
                 
         }
         return st.empty();
+        */
+       
+       // 2 Approach
+        stack<char> st;
+          for(auto &ch: s){
+              if(ch=='(')
+                  st.push(')');
+              else if(ch=='{')
+                  st.push('}');
+              else if(ch=='[')
+                  st.push(']');
+              else if(st.empty() || ch!=st.top())
+                 return false;
+              else
+                  st.pop();
+              
+          }
+       return st.empty();
     }
 };
