29b9864cc24c7e2f928b02793a96ceb0446fd27d
Maurya2811 <113296853+Maurya2811@users.noreply.github.com>
2023-04-11 20:10:49 +0530

    Time: 87 ms (62.09%), Space: 28.6 MB (6.74%) - LeetHub

--- 2390-removing-stars-from-a-string/2390-removing-stars-from-a-string.cpp
+++ 2390-removing-stars-from-a-string/2390-removing-stars-from-a-string.cpp
@@ -2,27 +2,11 @@ class Solution {
 public:
     string removeStars(string s) {
        // 3 approaches 
-         
-        //  1. Using Stack (Slow Approach)
-       /* 
-        stack<int> st;
-        for(int i=0;i<s.length();i++){
-            if(s[i]=='*'){
-                st.pop();
-            }
-            else 
-                st.push(s[i]);
-        }
-        string str ="";
-     while(!st.empty()){
-         str+=st.top();
-         st.pop();
-     }
-        reverse(str.begin(),str.end());
-       return str;
-       */
         
-      // 2nd Approach
+        
+        // 1 Approach 
+        // Using String
+     /* 
         string str="";
         for(int i=0;i<s.length();i++){
             if(s[i]=='*'){
@@ -32,5 +16,49 @@ public:
                 str.push_back(s[i]);
          }
         return str;
+     */
+           
+        //  2. Using Stack (Slow Approach)
+        
+       /* 
+        stack<int> st;
+        for(int i=0;i<s.length();i++){
+            if(s[i]=='*'){
+                st.pop();
+            }
+            else 
+                st.push(s[i]);
+        }
+        string result ="";
+     while(!st.empty()){
+         result.push_back(st.top());
+         st.pop();
+     }
+        reverse(result.begin(),result.end());
+       return result;
+       */
+        
+    // Using 2 pointer 
+        int size=s.length();
+        
+        vector<char> arr(size);
+        int j=0;
+        for(int i=0;i<size;i++){
+            if(s[i]=='*'){
+                j--;
+            }
+            else
+            {
+                arr[j]=s[i];
+                j++;
+            }
+        }
+        string result="";
+        for(int k=0;k<j;k++){
+            result.push_back(arr[k]);
+        }
+        
+        
+        return result;
     }
 };
