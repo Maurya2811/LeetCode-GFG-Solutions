f30bc9fa25c2d0ed4c20903a34f82be128c61b3d
Maurya2811 <113296853+Maurya2811@users.noreply.github.com>
2023-04-13 02:05:22 +0530

    Time: 9 ms (33.64%), Space: 11.5 MB (5.95%) - LeetHub

--- 0071-simplify-path/0071-simplify-path.cpp
+++ 0071-simplify-path/0071-simplify-path.cpp
@@ -1,11 +1,12 @@
 // 2 approaches
 //              1. Using Stack
-     
+//             2. Using String
    
 class Solution {
 public:
     
     string simplifyPath(string path) {
+   /*
         stringstream ss(path);
         string token="";
         stack<string> st;
@@ -33,6 +34,31 @@ public:
             ans = "/"+ st.top() + ans;
             st.pop();
         }
+        return ans;
+        */
+        
+  //             2. Using String
+
+                stringstream ss(path);
+        string token="";
+      vector <string>st;
+        while(getline(ss,token,'/')){
+            if(token == "." || token == "")
+                continue;
+            if(token!=".." )
+               {  
+                st.push_back(token);
+               }
+            else if (!st.empty())
+                st.pop_back();         
+            
+        }
+        if(st.empty())
+            return "/";
+        string ans="";
+       for(auto &it:st){
+           ans =ans + "/" + it;
+       }
         return ans;
     }
 };
