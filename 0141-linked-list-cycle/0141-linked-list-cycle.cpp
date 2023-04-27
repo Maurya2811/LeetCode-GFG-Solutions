4c7b63947ec2c493aba5df5c105925054c150284
Maurya2811 <113296853+Maurya2811@users.noreply.github.com>
2023-04-28 01:56:49 +0530

    Time: 12 ms (42.54%), Space: 8.2 MB (48.85%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.cpp
+++ 0141-linked-list-cycle/0141-linked-list-cycle.cpp
@@ -6,22 +6,41 @@
  *     ListNode(int x) : val(x), next(NULL) {}
  * };
  */
+// class Solution {
+// public:
+//     bool hasCycle(ListNode *head) {
+//         if(head==NULL)
+//             return false;
+//         if(head->next==NULL)
+//             return false;
+//         ListNode* slow=head;
+//         ListNode* fast=head;
+//         while(fast!=NULL && fast->next!=NULL){
+//             fast=fast->next->next;
+//             slow=slow->next;
+              
+//             if(fast==slow)
+//                 return true;
+//         }
+//         return false;
+//     }
+// };
 class Solution {
 public:
     bool hasCycle(ListNode *head) {
-        if(head==NULL)
+        if(head==NULL){
             return false;
-        if(head->next==NULL)
-            return false;
+        }
         ListNode* slow=head;
         ListNode* fast=head;
-        while(fast!=NULL && fast->next!=NULL){
-            fast=fast->next->next;
+        while(fast!=NULL && fast->next!=NULL ){
             slow=slow->next;
-              
-            if(fast==slow)
+            fast=fast->next->next;
+            if(slow==fast){
                 return true;
+            }
         }
         return false;
+        
     }
 };
