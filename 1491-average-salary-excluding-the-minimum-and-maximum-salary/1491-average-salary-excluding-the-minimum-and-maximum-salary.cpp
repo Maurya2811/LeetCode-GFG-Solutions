504f93086901c23e6e94973e3f4301f7f1ae3332
Maurya2811 <113296853+Maurya2811@users.noreply.github.com>
2023-05-02 01:48:54 +0530

    Time: 2 ms (46.45%), Space: 7.1 MB (83.65%) - LeetHub

--- 1491-average-salary-excluding-the-minimum-and-maximum-salary/1491-average-salary-excluding-the-minimum-and-maximum-salary.cpp
+++ 1491-average-salary-excluding-the-minimum-and-maximum-salary/1491-average-salary-excluding-the-minimum-and-maximum-salary.cpp
@@ -1,12 +1,21 @@
 class Solution {
 public:
     double average(vector<int>& salary) {
+  // 1. Brute Force 
+        /*
         sort(salary.begin(),salary.end());
         int n=salary.size();
         long long sum=0;
         for(int i=1;i<n-1;i++){
             sum+=salary[i];
         }
+        return (double)(sum)/(n-2);
+        */
+        int n=salary.size();
+        int max= *max_element(salary.begin(),salary.end());
+        int min= *min_element(salary.begin(),salary.end());
+        int sum=accumulate(salary.begin(),salary.end(),0);
+        sum=sum - (max+min);
         return (double)(sum)/(n-2);
     }
 };
