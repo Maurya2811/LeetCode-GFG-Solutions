459be615c4df1be560b476b94107568be0fcdd1d
Maurya2811 <113296853+Maurya2811@users.noreply.github.com>
2023-05-03 02:11:34 +0530

    Time: 180 ms (75.23%), Space: 6.9 MB (87.52%) - LeetHub

--- 0279-perfect-squares/0279-perfect-squares.cpp
+++ 0279-perfect-squares/0279-perfect-squares.cpp
@@ -1,3 +1,19 @@
+// Using Maths
+ /*
+     int numSquares(int n) {
+        if (ceil(sqrt(n))==floor(sqrt(n))) return 1;
+
+        int a = n;
+        while(a%4==0) a/=4;
+        if(a%8==7) return 4;
+
+        for(int i=1;i*i<=n;i++){
+            if(ceil(sqrt(n-i*i))==floor(sqrt(n-i*i))) return 2;
+        }
+
+        return 3;
+    }
+    */
 class Solution {
 public:
      int dp[10001];
@@ -31,4 +47,6 @@ public:
         
         
     }
+    
+   
 };
