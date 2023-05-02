f7bc44d5af293555d993df60a5130db119458734
Maurya2811 <113296853+Maurya2811@users.noreply.github.com>
2023-05-03 03:51:58 +0530

    Time: 39 ms (50.08%), Space: 8.4 MB (63.99%) - LeetHub

--- 1155-number-of-dice-rolls-with-target-sum/1155-number-of-dice-rolls-with-target-sum.cpp
+++ 1155-number-of-dice-rolls-with-target-sum/1155-number-of-dice-rolls-with-target-sum.cpp
@@ -7,7 +7,7 @@ public:
             return 1;
         // Valid mode
         if(n==0 && target!=0)
-            return 0;
+            return dp[n][target]=0;
         // Invalid Step
         
         if(dp[n][target]!=-1)
