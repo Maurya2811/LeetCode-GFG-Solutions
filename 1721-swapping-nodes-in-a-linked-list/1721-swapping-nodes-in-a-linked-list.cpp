5b34b58ec6188f09ebd907be27ea563095cea4fc
Maurya2811 <113296853+Maurya2811@users.noreply.github.com>
2023-05-15 17:21:16 +0530

    Time: 714 ms (21.21%), Space: 180.1 MB (54.58%) - LeetHub

--- 1721-swapping-nodes-in-a-linked-list/1721-swapping-nodes-in-a-linked-list.cpp
+++ 1721-swapping-nodes-in-a-linked-list/1721-swapping-nodes-in-a-linked-list.cpp
@@ -3,29 +3,63 @@ class Solution {
 public:
     ListNode* swapNodes(ListNode* head, int k) {
         ListNode* temp=head;
-        ListNode* firstNode;
+        ListNode* firstNode=head;
         ListNode* secondNode=head;
         
+        if(head->next==NULL)
+            return head;
         
-        int length=0;
+        int length=1;
         
-        while(temp!=NULL){
-            if(length+1==k){
+        while(temp->next!=NULL){
+            if(length==k){
                 firstNode = temp;
             }
+            
+            if(length>=k){
+                secondNode = secondNode->next;
+            }
             length++;
+            
             temp=temp->next;
         }
-        int count = length-k;
-        while(count){
-            secondNode = secondNode->next;
-            count--;
+        
+//        while(fast->next!=NULL){
+//             slow=slow->next;
+//             fast=fast->next;
+            
+//         }
+        
+        if(length==k){
+            firstNode=head;
+            secondNode=temp;
         }
         
-        // swap(firstNode->val,secondNode->val);
-        int t=firstNode->val;
-        firstNode->val = secondNode->val;
-        secondNode->val=t;
+        swap(firstNode->val,secondNode->val);
+        // int t=firstNode->val;
+        // firstNode->val = secondNode->val;
+        // secondNode->val=t;
+        
         return head;
     }
 };
+// class Solution {
+// public:
+//     ListNode* swapNodes(ListNode* head, int k) {
+//          ListNode* slow=head;
+//         ListNode* fast=head;
+//          ListNode* f;
+//         ListNode* l;
+//         while(--k){
+//             fast=fast->next;
+//         }
+//         f=fast;
+//         while(fast->next!=NULL){
+//             slow=slow->next;
+//             fast=fast->next;
+            
+//         }
+//         swap(slow->val,f->val);
+//         return head;
+//     }
+// };
