a443a5f28f532c41258ad66ea78a837d66514f7e
Maurya2811 <113296853+Maurya2811@users.noreply.github.com>
2023-05-16 00:37:28 +0530

    Time: 703 ms (30.96%), Space: 180 MB (99.96%) - LeetHub

--- 1721-swapping-nodes-in-a-linked-list/1721-swapping-nodes-in-a-linked-list.cpp
+++ 1721-swapping-nodes-in-a-linked-list/1721-swapping-nodes-in-a-linked-list.cpp
@@ -1,65 +1,69 @@
 
-class Solution {
-public:
-    ListNode* swapNodes(ListNode* head, int k) {
-        ListNode* temp=head;
-        ListNode* firstNode=head;
-        ListNode* secondNode=head;
-        
-        if(head->next==NULL)
-            return head;
-        
-        int length=1;
-        
-        while(temp->next!=NULL){
-            if(length==k){
-                firstNode = temp;
-            }
-            
-            if(length>=k){
-                secondNode = secondNode->next;
-            }
-            length++;
-            
-            temp=temp->next;
-        }
-        
-//        while(fast->next!=NULL){
-//             slow=slow->next;
-//             fast=fast->next;
-            
-//         }
-        
-        if(length==k){
-            firstNode=head;
-            secondNode=temp;
-        }
-        
-        // swap(firstNode->val,secondNode->val);
-        int t=firstNode->val;
-        firstNode->val = secondNode->val;
-        secondNode->val=t;
-        
-        return head;
-    }
-};
 // class Solution {
 // public:
 //     ListNode* swapNodes(ListNode* head, int k) {
-//          ListNode* slow=head;
-//         ListNode* fast=head;
-//          ListNode* f;
-//         ListNode* l;
-//         while(--k){
-//             fast=fast->next;
-//         }
-//         f=fast;
-//         while(fast->next!=NULL){
-//             slow=slow->next;
-//             fast=fast->next;
+//         ListNode* temp=head;
+//         ListNode* firstNode=head;
+//         ListNode* secondNode=head;
+        
+//         if(head->next==NULL)
+//             return head;
+        
+//         int length=1;
+        
+//         while(temp->next!=NULL){
+//             if(length==k){
+//                 firstNode = temp;
+//             }
             
+//             if(length>=k){
+//                 secondNode = secondNode->next;
+//             }
+//             length++;
+            
+//             temp=temp->next;
 //         }
-//         swap(slow->val,f->val);
+        
+        
+//         if(length==k){
+//             firstNode=head;
+//             secondNode=temp;
+//         }
+        
+//         // swap(firstNode->val,secondNode->val);
+//         int t=firstNode->val;
+//         firstNode->val = secondNode->val;
+//         secondNode->val=t;
+        
 //         return head;
 //     }
 // };
+
+class Solution {
+public:
+    ListNode* swapNodes(ListNode* head, int k) {
+         ListNode* p1=NULL;
+        ListNode* p2=NULL;
+        ListNode* temp=head;
+        while(temp){
+            
+            if(p2){
+                p2=p2->next;
+            }
+            
+           --k;
+            if(k==0){
+                p1=temp;
+                p2=head; // Activate
+            }
+            
+            temp=temp->next;
+            
+        }
+        // swap(p1->val,p2->val);
+        int t=p1->val;
+        p1->val = p2->val;
+        p2->val=t;
+        return head;
+    }
+};
