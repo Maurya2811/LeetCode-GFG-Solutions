141a68bfcd7393ff0cf114b8b18b53fe6cbfd55f
Maurya2811 <113296853+Maurya2811@users.noreply.github.com>
2023-05-25 23:18:51 +0530

    Time: 4 ms (68.55%), Space: 9.1 MB (85.48%) - LeetHub

--- 0837-new-21-game/0837-new-21-game.cpp
+++ 0837-new-21-game/0837-new-21-game.cpp
@@ -1,7 +1,8 @@
 class Solution {
 public:
     double new21Game(int n, int k, int maxPts) {
-         vector<double> dp(n + 1);
+      /*
+        vector<double> dp(n + 1);
         dp[0] = 1;
         double s = k > 0 ? 1 : 0;
         for (int i = 1; i <= n; i++) {
@@ -14,5 +15,19 @@ public:
             }
         }
         return accumulate(dp.begin() + k, dp.end(), 0.0);
+     */   
+        
+         if(n>=k+maxPts || k == 0) return 1.0;
+        vector<double>dp(n+1,0);
+        double pSum =1.0;
+        double res= 0.0;
+        dp[0] = 1.0;
+        for(int i=1;i<=n;i++){
+            dp[i] = pSum/maxPts;
+            if(i<k) pSum += dp[i];
+            else res += dp[i];
+            if(i-maxPts >=0) pSum -= dp[i-maxPts];
+        }
+        return res;
     }
 };
