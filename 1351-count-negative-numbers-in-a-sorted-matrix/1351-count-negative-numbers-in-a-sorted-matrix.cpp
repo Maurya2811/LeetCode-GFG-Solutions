26b77b9f41c11c7082c83e6d6a3da47a0597fa5d
Maurya2811 <113296853+Maurya2811@users.noreply.github.com>
2023-06-08 20:09:19 +0530

    Time: 18 ms (53.47%), Space: 10.4 MB (31.99%) - LeetHub

--- 1351-count-negative-numbers-in-a-sorted-matrix/1351-count-negative-numbers-in-a-sorted-matrix.cpp
+++ 1351-count-negative-numbers-in-a-sorted-matrix/1351-count-negative-numbers-in-a-sorted-matrix.cpp
@@ -1,28 +1,48 @@
-class Solution {
+// class Solution {
+// public:
+//     int search(int s, int e,vector<int> &ar){
+//         int ans_idx=-1;
+//         while(s<=e){
+//             int mid= s + (e-s)/2;
+//             if(ar[mid]<0){
+//                 ans_idx =mid;
+//                 e=mid-1;
+//             }
+//             else{
+//                 s=mid+1;
+//             }
+//         }
+//         return ans_idx;
+//     }
+//     int countNegatives(vector<vector<int>>& arr) {
+//         int count =0;
+//         int m=arr.size();
+//         int n=arr[0].size();
+        
+//         for(int i=0;i<m;i++){
+//             int temp = search(0,n-1,arr[i]);
+//             if(temp!=-1){
+//                 count+=n-temp;
+//             }
+//         }
+//         return count;
+//     }
+// };
+ class Solution {
 public:
-    int search(int s, int e,vector<int> &ar){
-        int ans_idx=-1;
-        while(s<=e){
-            int mid= s + (e-s)/2;
-            if(ar[mid]<0){
-                ans_idx =mid;
-                e=mid-1;
-            }
-            else{
-                s=mid+1;
-            }
-        }
-        return ans_idx;
-    }
+    
     int countNegatives(vector<vector<int>>& arr) {
         int count =0;
-        int m=arr.size();
-        int n=arr[0].size();
+        int row=arr.size()-1;
+        int col=arr[0].size();
+        int n=0;
         
-        for(int i=0;i<m;i++){
-            int temp = search(0,n-1,arr[i]);
-            if(temp!=-1){
-                count+=n-temp;
+        while(row>=0 && n<col){
+            if(arr[row][n]<0){
+                count+=col-n;
+                row--;
+            }else{
+                n++;
             }
         }
         return count;
