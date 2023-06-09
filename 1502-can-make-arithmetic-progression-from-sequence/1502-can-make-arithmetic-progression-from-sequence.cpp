6512c65b5105dcf74e13eedea3862bfc8926ef24
Maurya2811 <113296853+Maurya2811@users.noreply.github.com>
2023-06-10 02:35:23 +0530

    Update 1502-can-make-arithmetic-progression-from-sequence.cpp

--- 1502-can-make-arithmetic-progression-from-sequence/1502-can-make-arithmetic-progression-from-sequence.cpp
+++ 1502-can-make-arithmetic-progression-from-sequence/1502-can-make-arithmetic-progression-from-sequence.cpp
@@ -58,3 +58,50 @@ public:
     }
 };
 // */
+// Approach-3 (Using Mathematival AP property - O(n) Time, O(1) Space))
+
+class Solution {
+public:
+    bool canMakeArithmeticProgression(vector<int>& arr) {
+        int n = arr.size();
+        
+        
+        
+        int min_el = *min_element(begin(arr), end(arr)); // a
+        int max_el = *max_element(begin(arr), end(arr)); // a+(n-1)*d
+                                                         // -
+                                                        // ---------------------
+                                                        //   (n-1)*d = constt
+                                                       // d = constt /(n-1)
+                         // Since common differece never be fractional that's why 
+                         // n-1 must be multiple of diff( max_el - min_el )
+        
+        if((max_el - min_el)%(n-1)!=0)
+            return false;
+        
+        int d = (max_el - min_el)/(n-1);
+    
+        int i = 0; 
+        //a0 , a0+d , a0 + 2d
+        
+        while(i < n) {
+           int val = arr[i];
+            
+            if(val == min_el + i*d )
+                i++;
+           else{ 
+              if((val-min_el)%d!=0)
+                return false;
+            
+               int j=(val-min_el)/d ;
+            
+              if(arr[j]==val)
+                  return false;
+               
+               swap(arr[i],arr[j]);
+          }
+        }
+        
+        return true;
+    }
+};
