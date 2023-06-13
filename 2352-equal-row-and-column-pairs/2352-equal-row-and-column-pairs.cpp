f04148591a49dd09eda92b69af4d3442322af2c4
Maurya2811 <113296853+Maurya2811@users.noreply.github.com>
2023-06-13 17:35:53 +0530

    Time: 106 ms (71.56%), Space: 32.7 MB (47.26%) - LeetHub

--- 2352-equal-row-and-column-pairs/2352-equal-row-and-column-pairs.cpp
+++ 2352-equal-row-and-column-pairs/2352-equal-row-and-column-pairs.cpp
@@ -1,22 +1,47 @@
+// class Solution {
+// public:
+//     int equalPairs(vector<vector<int>>& grid) {
+//         int n=grid.size();
+//         bool flag = true;
+//        int count = 0;
+//         for(int i=0;i<n;i++){
+//             for(int j=0;j<n;j++){
+//                 flag = true;
+//                 for(int k=0;k<n;k++){
+//                     if(grid[i][k]!=grid[k][j]){
+//                         flag = false;
+//                         break;
+//                     }
+//                 }
+//                 if(flag){
+//                     count++;
+//                 }
+//             }
+//         }
+//         return count;
+//     }
+// };
+
+
+
 class Solution {
 public:
     int equalPairs(vector<vector<int>>& grid) {
-        int n=grid.size();
-        bool flag = true;
+        int n=grid.size();     
        int count = 0;
-        for(int i=0;i<n;i++){
-            for(int j=0;j<n;j++){
-                flag = true;
-                for(int k=0;k<n;k++){
-                    if(grid[i][k]!=grid[k][j]){
-                        flag = false;
-                        break;
-                    }
-                }
-                if(flag){
-                    count++;
-                }
+      
+        map<vector<int>,int> mp;
+        
+        for(int row =0; row<n ; row++){
+            mp[grid[row]]++;
+        }
+        
+        for(int c=0;c<n;c++){
+            vector<int> temp;
+            for(int r=0;r<n;r++){
+                temp.push_back(grid[r][c]);
             }
+            count+=mp[temp];
         }
         return count;
     }
