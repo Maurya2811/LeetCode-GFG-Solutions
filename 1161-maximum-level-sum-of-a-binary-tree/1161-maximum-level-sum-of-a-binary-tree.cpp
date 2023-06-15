f9b02078c8463a15e88bbbb246764f7ad4dcc42a
Maurya2811 <113296853+Maurya2811@users.noreply.github.com>
2023-06-15 19:52:57 +0530

    Time: 233 ms (5.76%), Space: 104.8 MB (90.73%) - LeetHub

--- 1161-maximum-level-sum-of-a-binary-tree/1161-maximum-level-sum-of-a-binary-tree.cpp
+++ 1161-maximum-level-sum-of-a-binary-tree/1161-maximum-level-sum-of-a-binary-tree.cpp
@@ -1,45 +1,68 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
+// // BFS
+// class Solution {
+// public:
+//     int maxLevelSum(TreeNode* root) {
+//         queue<TreeNode*> que;
+//         que.push(root);
+//         int resultLevel=0;
+//         int currLevel =0;
+//         int maxSum=INT_MIN;
+       
+//         while(!que.empty()){
+//             int n=que.size();
+//             int sum=0;
+//             while(n--){
+                
+//                 TreeNode* temp = que.front();
+//                 que.pop();
+//                 sum += temp->val;
+//                 if(temp->left)
+//                 que.push(temp->left);
+//                 if(temp->right)
+//                 que.push(temp->right);
+                
+//             }
+//             currLevel++;
+            
+//             if(sum> maxSum){
+//                 maxSum = sum;
+//                 resultLevel=currLevel;
+//             }
+            
+//         }
+//         return resultLevel;
+//     }
+// };
+
+// DFS
 class Solution {
 public:
+    map<int,int> mp;
+    
+    void solve(TreeNode* root, int level){
+        if(!root)
+            return;
+        
+        solve(root->left,level+1);
+          mp[level]+=root->val;
+        solve(root->right,level+1);
+        
+    }
+    
     int maxLevelSum(TreeNode* root) {
-        queue<TreeNode*> que;
-        que.push(root);
-        int resultLevel=0;
-        int currLevel =0;
-        int maxSum=INT_MIN;
-       
-        while(!que.empty()){
-            int n=que.size();
-            int sum=0;
-            while(n--){
-                
-                TreeNode* temp = que.front();
-                que.pop();
-                sum =sum + temp->val;
-                if(temp->left)
-                que.push(temp->left);
-                if(temp->right)
-                que.push(temp->right);
-                
+        int max=INT_MIN;
+        int sum=0;
+    
+        int finalLevel=0;
+       solve(root,1);
+        for(auto &x:mp){
+            sum=x.second;
+           
+            if(sum>max){
+                max=sum;
+                finalLevel=x.first;
             }
-            currLevel++;
-            
-            if(sum> maxSum){
-                maxSum = sum;
-                resultLevel=currLevel;
-            }
-            
         }
-        return resultLevel;
+        return finalLevel;
     }
 };
