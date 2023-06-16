8eef5149477c3eea7f6418a9c81d0e85bbf66137
Maurya2811 <113296853+Maurya2811@users.noreply.github.com>
2023-06-16 16:58:39 +0530

    Time: 374 ms (39.73%), Space: 163.2 MB (46.36%) - LeetHub

--- 1569-number-of-ways-to-reorder-array-to-get-same-bst/1569-number-of-ways-to-reorder-array-to-get-same-bst.cpp
+++ 1569-number-of-ways-to-reorder-array-to-get-same-bst/1569-number-of-ways-to-reorder-array-to-get-same-bst.cpp
@@ -1,41 +1,44 @@
 class Solution {
 public:
+    // typedef long long ll;
+    #define ll long long
+    ll MOD = 1e9 + 7;
+    vector<vector<ll>>PT;
+    
+      int solve(vector<int>& nums){
+          int n = nums.size();
+          if(n<3)
+              return 1;
+          
+          vector<int> leftArray , rightArray ;
+          
+          int root = nums[0];
+          
+          for(int i=1;i<n;i++){
+              
+              if(nums[i]<root)
+                  leftArray.push_back(nums[i]);
+              else
+                  rightArray.push_back(nums[i]);
+              
+          }
+          ll x = solve(leftArray)% MOD;
+          ll y = solve(rightArray)% MOD;
+          ll z= PT[n-1][leftArray.size()];
+          
+          return (((x*y)%MOD*z))%MOD     ;      
+      }
     int numOfWays(vector<int>& nums) {
-        int m = nums.size();
-        // Table of Pascal's triangle
-        table.resize(m + 1);
-        for(int i = 0; i < m + 1; ++i) {
-            table[i] = vector<long long>(i + 1, 1);
-            for(int j = 1; j < i; ++j) {
-                table[i][j] = (table[i - 1][j - 1] + table[i - 1][j]) % mod;
-            }
+        int n = nums.size();
+       PT.resize(n+1) ;
+        for(int i =0; i<= n; i++){
+            
+                 PT[i]= vector<ll>(i+1,1) ; 
+            
+                for(int j=1;j<i;j++){
+                        PT[i][j]= (PT[i-1][j-1]+PT[i-1][j])%MOD;
+                }
         }
-        
-        return (dfs(nums) - 1) % mod;
-    }
-    
-private:
-    vector<vector<long long>> table;
-    long long mod = 1e9 + 7;
-    
-    long long dfs(vector<int> &nums){
-        int m = nums.size();
-        if(m < 3) {
-            return 1;
-        }
-
-        vector<int> leftNodes, rightNodes;
-        for(int i = 1; i < m; ++i){
-            if (nums[i] < nums[0]) {
-                leftNodes.push_back(nums[i]);
-            } else {
-                rightNodes.push_back(nums[i]);
-            }
-        }
-		
-        long long leftWays = dfs(leftNodes) % mod;
-        long long rightWays = dfs(rightNodes) % mod;
-		
-        return (((leftWays * rightWays) % mod) * table[m - 1][leftNodes.size()]) % mod;
+        return (solve(nums)-1)%MOD;
     }
 };
