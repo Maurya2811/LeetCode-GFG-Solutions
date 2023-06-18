74198ed22de8b768018092ff9468ba358100d1d0
Maurya2811 <113296853+Maurya2811@users.noreply.github.com>
2023-06-18 16:30:38 +0530

    Time: 1192 ms (7.33%), Space: 186 MB (6.06%) - LeetHub

---
+++ 2328-number-of-increasing-paths-in-a-grid/2328-number-of-increasing-paths-in-a-grid.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    long long MOD =1e9 +7;
+    vector<vector<int>>directions{
+        {0,-1},{0,1},{1,0},{-1,0}
+    };
+    
+    int m,n;
+    int t[1001][1001];
+     bool isSafe(int i,int j){
+         return (i<m && i>=0 && j<n && j>=0);
+     }  
+    int dfs(vector<vector<int>>& grid,int i,int j){
+        int answer =1;
+       if(t[i][j]!=-1)
+           return t[i][j];
+        for(auto dir:directions){
+            int i_ = i+ dir[0];
+            int j_ = j+ dir[1];
+            if(isSafe(i_,j_) && grid[i_][j_]<grid[i][j])
+            answer = answer + dfs(grid,i_,j_);
+        }
+        
+        return t[i][j]=(answer)%MOD;
+    }
+    int countPaths(vector<vector<int>>& grid) {
+        m=grid.size();
+        n=grid[0].size();
+        memset(t,-1,sizeof(t));
+       int result =0;
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                 result =( (result%MOD) + dfs(grid,i,j))%MOD;
+            }
+        }
+        return result%MOD;
+    }
+};
