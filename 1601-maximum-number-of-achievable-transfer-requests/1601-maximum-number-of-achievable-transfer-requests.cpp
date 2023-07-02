7da6a3952e877c055532bdd40aa6dbfa32d5de4e
Maurya2811 <113296853+Maurya2811@users.noreply.github.com>
2023-07-03 04:18:12 +0530

    Time: 82 ms (92.59%), Space: 8.7 MB (75.93%) - LeetHub

--- 1601-maximum-number-of-achievable-transfer-requests/1601-maximum-number-of-achievable-transfer-requests.cpp
+++ 1601-maximum-number-of-achievable-transfer-requests/1601-maximum-number-of-achievable-transfer-requests.cpp
@@ -1,42 +1,35 @@
 class Solution {
 public:
-    int maximumRequests(int n, vector<vector<int>>& requests) {
-        int answer = 0; 
-        
-        for (int mask = 0; mask < (1 << requests.size()); mask++) {
-            vector<int> indegree(n, 0);
-            int pos = requests.size() - 1;
-            // Number of set bits representing the requests we will consider.
-            int bitCount = __builtin_popcount(mask);
-            
-            // If the request count we're going to consider is less than the maximum request 
-            // We have considered without violating the constraints; then we can return it cannot be the answer.
-            if (bitCount <= answer) {
-                continue;
+    int result = INT_MIN;
+    
+    void solve(int idx,int count, vector<vector<int>>& requests, vector<int>&resultant ){
+        if(idx>= requests.size()){
+            for(int x:resultant){
+                if(x!=0)
+                    return;
             }
-            
-            // For all the 1's in the number, update the array indegree for the building it involves.
-            for (int curr = mask; curr > 0; curr >>= 1, pos--) {
-                if (curr & 1) {
-                    indegree[requests[pos][0]]--;
-                    indegree[requests[pos][1]]++;
-                }
-            }
-            
-            int flag = 1;
-            // Check if it doesn;t violates the constraints
-            for (int i = 0; i < n; i++) {
-                if (indegree[i]) {
-                    flag = 0;
-                    break;
-                }
-            }
-            
-            if (flag)  {
-                answer = bitCount;
-            }
+            result = max(result,count);
+            return;
         }
         
-        return answer;
+        int from = requests[idx][0];
+        int to = requests[idx][1];
+        
+        resultant[from]--;
+        resultant[to]++;
+
+       solve(idx+1,count+1,requests,resultant);
+        
+        resultant[from]++;
+        resultant[to]--;
+        solve(idx+1,count,requests,resultant);
+        
+        
+    }
+    int maximumRequests(int n, vector<vector<int>>& requests) {
+        vector<int> resultant(n,0);
+        
+        solve(0,0,requests,resultant);
+        return result;
     }
 };
