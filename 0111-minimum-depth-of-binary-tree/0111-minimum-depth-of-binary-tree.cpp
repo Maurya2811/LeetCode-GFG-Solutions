8f687ebe067540bbcd57dad19ef37bcd735fa354
Rahul Maurya <113296853+Maurya2811@users.noreply.github.com>
2023-07-10 19:23:42 +0530

    Time: 319 ms (10.78%), Space: 146.3 MB (7.51%) - LeetHub

--- 0111-minimum-depth-of-binary-tree/0111-minimum-depth-of-binary-tree.cpp
+++ 0111-minimum-depth-of-binary-tree/0111-minimum-depth-of-binary-tree.cpp
@@ -1,30 +1,55 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
+ // Using BFS
 
+// Use this type of BFS when minimum depth is needed
+/*
 class Solution {
 public:
-    int findDepth(TreeNode* root){
-        if(!root)
-            return 0;
-     if(!root->left && !root->right)
-         return 1;
-     if(!root->left || !root->right)
-     return 1+max(findDepth(root->left),findDepth(root->right));
-       
-      return 1+ min(findDepth(root->left),findDepth(root->right));
-    }
-     
-    
     int minDepth(TreeNode* root) {
-      return findDepth (root);
+          if(!root)
+               return 0;
+        queue<TreeNode*> que;
+        que.push(root);
+        int depth =1;
+        while(!que.empty()){
+            
+            int n= que.size();  
+            
+            while(n--){
+                TreeNode* temp = que.front();
+                que.pop();
+                
+                if(!temp->left && !temp->right){
+                    return depth;
+                }
+                
+                if(temp->left){
+                    que.push(temp->left);
+                }
+                 if(temp->right){
+                    que.push(temp->right);
+                }
+            }
+            depth++;        
+        }
+        return -1;
     }
 };
+*/
+// Using DFS
+class Solution {
+public:
+    int minDepth(TreeNode* root) {
+          if(!root)
+               return 0;
+          if(!root->left && !root->right ){
+                    return 1;
+                }
+        int left_depth = root->left ? minDepth(root->left) : INT_MAX;
+        int right_depth = root->right ? minDepth(root->right) : INT_MAX;
+        
+        return 1 + min(left_depth ,right_depth);
+    }
+};
+
+
+
