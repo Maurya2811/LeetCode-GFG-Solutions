42b6c0b9707cb6690945d43a6d19c108e78e6504
Rahul Maurya <113296853+Maurya2811@users.noreply.github.com>
2023-07-19 23:21:12 +0530

    Time: 472 ms (75.83%), Space: 89.7 MB (58.83%) - LeetHub

--- 0435-non-overlapping-intervals/0435-non-overlapping-intervals.cpp
+++ 0435-non-overlapping-intervals/0435-non-overlapping-intervals.cpp
@@ -1,35 +1,82 @@
+//Approach-1
+/*
 class Solution {
 public:
     int eraseOverlapIntervals(vector<vector<int>>& intervals) {
-          sort(begin(intervals),end(intervals));
-        int n = intervals.size();
-        int i=0,j=1;
-        int count =0;
+        sort(begin(intervals), end(intervals));
+        int n = intervals.size(); 
         
-        while(j<n){
-            int currStart = intervals[i][0];
-             int currEnd = intervals[i][1];
-            int nextStart = intervals[j][0];
-             int nextEnd = intervals[j][1];
+        int count = 0;
+        int i = 0, j = 1;
+        
+        while(j < n) {
+            vector<int> curr_interval = intervals[i];
+            vector<int> next_interval = intervals[j];
             
-            if(currEnd <= nextStart) // Safe
-            {
-               i=j;
-               
+            int curr_start = curr_interval[0];
+            int curr_end   = curr_interval[1];
+            
+            int next_start = next_interval[0];
+            int next_end   = next_interval[1];
+            
+            if(curr_end <= next_start) { //SAFE
+                i = j;
+                j++;
+            } else if(curr_end <= next_end) {
                 
-            }
-            else if(currEnd > nextEnd ){
-                i=j;
+                    // GREEDY : Remove next interval as it has high chances to
+                             // overlap with future intervals
+             
+                j++;
                 count++;
-                    
-            }
-            else if(nextEnd >= currEnd){
-                 
+            } else if(curr_end > next_end) {
+              
+                    // GREEDY : Remove current interval as it has high chances to
+                             // overlap with future intervals
+           
+                i = j;
+                j++;
                 count++;
             }
-            j++;
-            
         }
+
         return count;
+
+    }
+};
+*/
+
+//Approach-2 (Same as 1st Approach, Just different implementation)
+
+class Solution {
+public:
+    int eraseOverlapIntervals(vector<vector<int>>& intervals) {
+        sort(begin(intervals), end(intervals));
+        int n = intervals.size(); 
+        
+        int count = 0;
+        int i = 1;
+        vector<int> lastInterval = intervals[0];
+        while(i < n)  {
+            
+            int curr_end   = intervals[i][1];
+            int curr_start = intervals[i][0];
+            
+            int last_end = lastInterval[1];
+            
+            if(curr_start >= last_end) {
+                lastInterval = intervals[i]; //safe
+            } else if(curr_end >= last_end) {
+                count++; //Being Greedy
+            } else if(curr_end < last_end) {
+                lastInterval = intervals[i]; //Being Greedy
+                count++;
+            }
+            
+            i++;
+        }
+
+        return count;
+
     }
 };
