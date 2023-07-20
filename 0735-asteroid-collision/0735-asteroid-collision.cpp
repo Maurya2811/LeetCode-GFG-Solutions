1cbb79142cb032e1ca32e4057c06f77310015a46
Rahul Maurya <113296853+Maurya2811@users.noreply.github.com>
2023-07-20 17:45:29 +0530

    Time: 14 ms (74.27%), Space: 17.5 MB (85.34%) - LeetHub

--- 0735-asteroid-collision/0735-asteroid-collision.cpp
+++ 0735-asteroid-collision/0735-asteroid-collision.cpp
@@ -1,38 +1,34 @@
 //Using Stack
 class Solution {
-public:
-    vector<int> asteroidCollision(vector<int>& asteroids) {
-        vector<int> ans;
-        int n = asteroids.size();
-        ans.push_back(asteroids[0]);
-        for(int i=1;i<n;i++){
-             if(ans.size()==0){
-                  ans.push_back(asteroids[i]);
-                 continue;
-            }
-           int asteroid = asteroids[i];
-            int top = ans.back();
-           
-            if(asteroid <0 && top <0 || asteroid >0 && top >0 ){ // Both are moving in same directions
-                ans.push_back(asteroid);
-            }
-            else if(top>0 && asteroid<0){ // Collison Occurs
-                    
-                      int sum = top+asteroid ;
-                     if(sum==0){
-                         ans.pop_back();
-                     }
-                else if(sum<0){
-                    ans.pop_back();
-                    i--;
-                }
-               
-                }
-            else{
-                 ans.push_back(asteroid);
-            }
+  public: vector < int > asteroidCollision(vector < int > & asteroids) {
+    vector < int > ans;
+    int n = asteroids.size();
+    ans.push_back(asteroids[0]);
+    for (int i = 1; i < n; i++) {
+      if (ans.size() == 0) {
+        ans.push_back(asteroids[i]);
+        continue;
+      }
+      int asteroid = asteroids[i];
+      int top = ans.back();
+
+      if (asteroid < 0 && top < 0 || asteroid > 0 && top > 0) { // Both are moving in same directions
+        ans.push_back(asteroid);
+      } else if (top > 0 && asteroid < 0) { // Collison Occurs
+
+        int sum = top + asteroid;
+        if (sum == 0) {
+          ans.pop_back();
+        } else if (sum < 0) {
+          ans.pop_back();
+          i--;
         }
-         // reverse(ans.begin(),ans.end());
-        return ans;
+
+      } else {
+        ans.push_back(asteroid);
+      }
     }
+
+    return ans;
+  }
 };
