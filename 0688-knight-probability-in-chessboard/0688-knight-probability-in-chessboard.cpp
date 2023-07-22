253e686abfefcd42239a607295ddb43f5ab2920f
Rahul Maurya <113296853+Maurya2811@users.noreply.github.com>
2023-07-22 21:04:27 +0530

    Time: 416 ms (11.14%), Space: 17.1 MB (19.49%) - LeetHub

--- 0688-knight-probability-in-chessboard/0688-knight-probability-in-chessboard.cpp
+++ 0688-knight-probability-in-chessboard/0688-knight-probability-in-chessboard.cpp
@@ -1,3 +1,41 @@
+//Using simple recursion and memoization (T.C : We visit each state once only and we have k possible tries - O(k * n^2)
+class Solution {
+public:
+    unordered_map<string, double> mp;
+    vector<pair<int, int>> directions = {{1, 2}, {1, -2}, {-1, 2}, {-1, -2}, {2, 1}, {2, -1}, {-2, 1}, {-2, -1}};
+
+    double helper( int N, int K , int row, int col ) {
+        
+        if ( row < 0 || row >= N || col < 0 || col >= N ) 
+            return 0; 
+        
+        if (K==0)
+            return 1;  //one possibility over, return now
+        
+        string key = to_string(K) + "_" + to_string(row) + "_" + to_string(col);
+        
+        if(mp.find(key) != mp.end()) 
+            return mp[key];
+        
+        double ans = 0;
+        for(auto &dir : directions) {
+            int new_row = row + dir.first;
+            int new_col = col + dir.second;
+            ans += (double)helper(N, K-1, new_row, new_col);
+        }
+        
+        return mp[key] = (double)(ans/8.0); 
+    }
+    
+    
+    double knightProbability(int n, int k, int row, int column) {
+        return helper( n , k , row , column); 
+    }
+};
+
+
+// Simple Code
+/*
 class Solution {
 public:
     double dp[30][30][110];
@@ -23,3 +61,5 @@ public:
         return recur(n,k,row,column);
     }
 };
+
+*/
