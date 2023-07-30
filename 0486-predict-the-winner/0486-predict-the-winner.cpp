4647855dd225b40cd2668bd72c1309729ac06653
Rahul Maurya <113296853+Maurya2811@users.noreply.github.com>
2023-07-30 07:52:34 +0530

    Time: 0 ms (100.00%), Space: 7.3 MB (74.24%) - LeetHub

--- 0486-predict-the-winner/0486-predict-the-winner.cpp
+++ 0486-predict-the-winner/0486-predict-the-winner.cpp
@@ -1,14 +1,33 @@
 class Solution {
 public:
+    int dp[21][21];
+    int solve(vector<int>& nums,int i,int j){
+         int player2;
+        if(i>j)
+            return 0;
+        if(i==j)
+            return nums[i];
+        
+        if(dp[i][j]!=-1)
+            return dp[i][j];
+        
+        int take_left = nums[i]+ min(solve(nums,i+2,j), solve(nums,i+1,j-1));
+       int take_right = nums[j]+ min(solve(nums,i+1,j-1), solve(nums,i,j-2));
+        
+        int player1 = max(take_left,take_right);
+        
+        return dp[i][j]=player1;
+    }
+    
     bool PredictTheWinner(vector<int>& nums) {
+        
+        memset(dp,-1,sizeof(dp));
         int n = nums.size();
-        vector<vector<int>> dp(n, vector<int>(n)); // use to keep the score gap between player1 and player2
-        for (int i = 0; i < n; i++) dp[i][i] = nums[i];
-        for (int i = 1; i < n; i++) {
-            for (int j = 0; j+i < n; j++) {
-                dp[j][j+i] = max(nums[j+i]-dp[j][j+i-1], nums[j]-dp[j+1][j+i]);
-            }
-        }
-        return dp[0][n-1] >= 0; // player1 get more score points than player2
+       int  player2 = accumulate(nums.begin(),nums.end(),0) ;
+          int player1 = solve(nums,0,n-1);
+        
+        player2 = player2 - player1;
+        
+         return player1>= player2;
     }
 };
