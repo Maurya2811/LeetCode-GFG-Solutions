5b524558677626da218dc245ff9e7938bedd92aa
Rahul Maurya <113296853+Maurya2811@users.noreply.github.com>
2023-07-30 07:10:17 +0530

    Time: 7 ms (63.98%), Space: 138.4 MB (24.14%) - LeetHub

--- 0808-soup-servings/0808-soup-servings.cpp
+++ 0808-soup-servings/0808-soup-servings.cpp
@@ -1,27 +1,40 @@
 class Solution {
 public:
-    double soupServings(int n) {
-        int m = ceil(n / 25.0);
-        unordered_map<int, unordered_map<int, double>> dp;
-
-        function<double(int, int)> calculateDP = [&](int i, int j) -> double {
-            return (dp[max(0, i - 4)][j] + dp[max(0, i - 3)][j - 1] +
-                    dp[max(0, i - 2)][max(0, j - 2)] + dp[i - 1][max(0, j - 3)]) /
-                   4;
-        };
-
-        dp[0][0] = 0.5;
-        for (int k = 1; k <= m; k++) {
-            dp[0][k] = 1;
-            dp[k][0] = 0;
-            for (int j = 1; j <= k; j++) {
-                dp[j][k] = calculateDP(j, k);
-                dp[k][j] = calculateDP(k, j);
-            }
-            if (dp[k][k] > 1 - 1e-5) {
-                return 1;
-            }
+    
+    vector<pair<int,int>>serves ={{100,0},{75,25},{50,50},{25,75}};
+    vector<vector<double>> dp;
+    double find_probability(int A, int B){
+        
+         if(A<=0 && B<=0)
+             return 0.5;
+         if(A<=0)
+             return 1;
+        if(B<=0)
+            return 0;
+        
+        if(dp[A][B]!=-1.0)
+            return dp[A][B];
+        
+        double probability =0.0;
+        
+        for(auto x:serves){
+           int taking_A = A-x.first;
+           int taking_B = B-x.second;
+           probability += find_probability(taking_A,taking_B);
+            
         }
-        return dp[m][m];
+        
+        return dp[A][B]=0.25*probability;
+        
+    }
+    double soupServings(int n) {
+
+ 
+        if(n>=5000)         // Probability of A will be almost 1 if n value if so large
+            return 1.0;
+        
+       dp.resize(n+1,vector<double>(n+1,-1.0));
+        
+        return find_probability(n,n);
     }
 };
