442412515d834cfd73d65eb8dae5c03b3eef10e3
Rahul Maurya <113296853+Maurya2811@users.noreply.github.com>
2023-08-04 20:03:54 +0530

    Time: 32 ms (17.17%), Space: 17.8 MB (12.02%) - LeetHub

---
+++ 0139-word-break/0139-word-break.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+     
+    int n;
+    int dp[301];
+    bool solve(string &s,unordered_set<string> &st,int idx ){
+        if(idx>=n){
+            return true;
+        }
+          if(dp[idx]!=-1)
+              return dp[idx];
+        for(int i=1;i<=n;i++){
+            
+            string temp = s.substr(idx,i);
+            if(st.find(temp)!=st.end() && solve(s,st,idx+i)){
+                return dp[idx]= true;
+            }       
+        }
+        
+        return dp[idx]= false;
+    }
+    
+    bool wordBreak(string s, vector<string>& wordDict) {
+        n = s.length();
+        unordered_set<string>st;
+        for(string word: wordDict){
+            st.insert(word);
+        }
+        if(st.find(s)!=st.end()){
+            return true;
+        }
+      memset(dp,-1,sizeof(dp));
+        
+        
+        return solve(s,st,0);
+        
+    }
+};
