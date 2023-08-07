978533468d49f4dcc7d70447b5e2764dc0bb9cae
Rahul Maurya <113296853+Maurya2811@users.noreply.github.com>
2023-08-08 00:02:19 +0530

    Time: 13 ms (86.21%), Space: 26.3 MB (37.28%) - LeetHub

--- 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.cpp
+++ 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.cpp
@@ -1,25 +1,18 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
+
 class Solution {
 public:
     int n;
+    unordered_map<int,int> mp;
     int findPos(vector<int>& inorder,int inStart,int inEnd ,int val){
         
-        for(int i=inStart;i<=inEnd;i++){
-            if(inorder[i]==val){
-                return i;
-            }
-        }
-        return -1;
+        // for(int i=inStart;i<=inEnd;i++){
+        //     if(inorder[i]==val){
+        //         return i;
+        //     }
+        // }
+        // return -1;
+        
+        return mp[val];
     }
       TreeNode* solve(vector<int>& preorder, vector<int>& inorder,int &preIndex,int inStart,int inEnd){
           if(preIndex >= n || inStart>inEnd){
@@ -36,7 +29,11 @@ public:
           
       }
     TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
-           n=preorder.size();
+        n=preorder.size();
+        for(int x=0;x<n;x++){
+            mp[inorder[x]]=x;
+        }
+          
         int preIndex=0;
      return solve(preorder,inorder,preIndex,0,n-1);
 
