3efaaeac38c7171615fdbe0ff44e998c5b030ea8
Rahul Maurya <113296853+Maurya2811@users.noreply.github.com>
2023-08-07 21:42:50 +0530

    Added solution - LeetHub

--- Solve the Sudoku - GFG/solve-the-sudoku.cpp
+++ Solve the Sudoku - GFG/solve-the-sudoku.cpp
@@ -11,132 +11,55 @@ using namespace std;
 
 // } Driver Code Ends
 class Solution 
-{
-    public:
-    
-      bool isSafe(int row, int col, int board[N][N], int value) {
-        int n =9;
-
-        for(int i=0; i<n; i++) {
-            //row check
-            if(board[row][i] == value)
-                return false;
-            
-            //col check
-            if(board[i][col] == value) 
-                return false;
-
-            //3*3 box check
-   /*
-            for Indexing
-                         3*(row/3)+(i/3)
-            
-              3*(row/3) -> 0 , 3 , 6              
-           *   i/3      ->         0   
-                         +
-                        -------------
-                           0 , 3 , 6 
-                           
-               3*(row/3) -> 0 , 3 , 6
-           *   i/3      ->          1   
-                         +
-                        ---------------
-                            1 , 4 , 7  
-                           
-                           
-               3*(row/3) -> 0 , 3 , 6
-           *  i/3        ->         2   
-                         +
-                        -----------------
-                            2 , 5 , 8           
-                            
-            
-                           
-  ===================================================================              
-             FOR MOVEMENT 
-                 3*(col/3)+(i%3)
-                 
-                 
-               3*(col/3) -> 0 , 3 , 6         
-           *   i%3       ->         0   
-                         +
-                        -------------
-                           0 , 3 , 6 
-                           
-                           
-               3*(col/3) -> 0 , 3 , 6
-           *   i%3       ->         1   
-                         +
-                        ---------------
-                            1 , 4 , 7  
-                           
-                           
-               3*(col/3) -> 0 , 3 , 6
-           *  i%3        ->         2   
-                         +
-                        -----------------
-                            2 , 5 , 8       
-                            
-                            
-  */         
-              
-            if(board[3*(row/3)+(i/3)][3*(col/3)+(i%3)] == value) 
-                return false;
-        }
-
-        return true;
+{   private:
+    bool isSafe(int row, int col,int grid[N][N],int val){
+         for(int i=0;i<9;i++){
+             if(grid[i][col]==val){
+                 return false;
+             }
+             if(grid[row][i]==val){
+                 return false;
+             }
+             if(grid[3*(row/3)+ i/3][3* (col/3)+ i%3]==val){
+                 return false;
+             }
+         }
+         return true;
     }
-    
-    
+    public:
     //Function to find a solved Sudoku. 
-    bool SolveSudoku(int board[N][N])  
+    bool SolveSudoku(int grid[N][N])  
     { 
-        int n = 9;
-
-        for(int i=0; i<n; i++) {
-            for(int j=0; j<n; j++) {
-                //check for empty cell
-                if(board[i][j] == 0) {
-                    //try to fill with values ranging from 1 to 9
-                    for(char val = 1; val<=9; val++) {
-                        //check for safety
-                        if(isSafe(i,j, board, val)) {
-                            //insert
-                            board[i][j] = val;
-                            //recursion sambal lega
-                            bool remainingBoardSolution = SolveSudoku(board);
-                            if(remainingBoardSolution == true) {
-                                return true;
-                            }
-                            else {
-                                //backtrack
-                                board[i][j] = 0;
-                            }
-                        }
-                    }
-                    //if 1 se 9 tak koi bhi value se solution
-                    //nahi nikla ,current cell pr, 
-                    //that means piche kahin pr galti h , 
-                    //go back by returning false
-                return false;
-                }
+       for(int row=0;row<9;row++){
+            for(int col=0;col<9;col++){
+                if(grid[row][col]==0)
+                   {
+                       for(int i=1;i<=9;i++){
+                           if(isSafe(row,col,grid,i)){
+                               grid[row][col]=i;
+                              
+                              if(SolveSudoku(grid)){
+                                  return true;
+                              }
+                              grid[row][col]=0;
+                           }
+                       }
+                      return false; 
+                   }
             }
-        }
-        //all cells filled 
-        return true;
+       }
+       return true;
+       
     }
     
     //Function to print grids of the Sudoku.
     void printGrid (int grid[N][N]) 
     {
-       for(int i=0;i<9;i++){
-           for(int j=0;j<9;j++){
-               cout<<grid[i][j]<<" ";
-           }
-        //   if(i!=8){
-        //       cout<<endl;
-        //   }
-       }
+         for(int i=0;i<9;i++){
+             for(int j=0;j<9;j++){
+                 cout<<grid[i][j]<<" ";
+             }
+         } 
     }
 };
 
