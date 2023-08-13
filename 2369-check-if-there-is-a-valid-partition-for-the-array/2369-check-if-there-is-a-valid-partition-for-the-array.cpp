488f4c3c323e84ba6cb01b2921e629b223294a48
Rahul Maurya <113296853+Maurya2811@users.noreply.github.com>
2023-08-13 17:19:33 +0530

    Time: 119 ms (94.77%), Space: 93.1 MB (15.08%) - LeetHub

--- 2369-check-if-there-is-a-valid-partition-for-the-array/2369-check-if-there-is-a-valid-partition-for-the-array.cpp
+++ 2369-check-if-there-is-a-valid-partition-for-the-array/2369-check-if-there-is-a-valid-partition-for-the-array.cpp
@@ -15,14 +15,20 @@ public:
         if(idx+1<n){
              if(nums[idx]==nums[idx+1]){
                  rule1= solve(idx+2,nums,dp);
+                 if(rule1)
+                     return true;
              }
             
           if(idx+2<n) { 
               if(nums[idx]==nums[idx+1] && nums[idx]==nums[idx+2]){
                  rule2= solve(idx+3,nums,dp);
+                  if(rule2)
+                     return true;
                }
              if(nums[idx]+1==nums[idx+1] && nums[idx]+2==nums[idx+2]){
                 rule3= solve(idx+3,nums,dp);
+                 if(rule3)
+                     return true;
             }
             }
             return dp[idx] = (rule1 || rule2 || rule3) ;
