5372fd35ab6576655bc920a2e6dad4ad19eda867
Rahul Maurya <113296853+Maurya2811@users.noreply.github.com>
2023-08-13 17:11:35 +0530

    Time: 118 ms (94.77%), Space: 92.2 MB (37.85%) - LeetHub

---
+++ 2369-check-if-there-is-a-valid-partition-for-the-array/2369-check-if-there-is-a-valid-partition-for-the-array.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    
+    int n;
+    
+    bool solve(int idx,vector<int> & nums,vector<int>&dp){
+        if(idx>= n)
+            return true;
+        
+        if(dp[idx]!=-1){
+            return dp[idx];
+        }
+        
+        bool rule1=false, rule2=false,rule3=false;
+        if(idx+1<n){
+             if(nums[idx]==nums[idx+1]){
+                 rule1= solve(idx+2,nums,dp);
+             }
+            
+          if(idx+2<n) { 
+              if(nums[idx]==nums[idx+1] && nums[idx]==nums[idx+2]){
+                 rule2= solve(idx+3,nums,dp);
+               }
+             if(nums[idx]+1==nums[idx+1] && nums[idx]+2==nums[idx+2]){
+                rule3= solve(idx+3,nums,dp);
+            }
+            }
+            return dp[idx] = (rule1 || rule2 || rule3) ;
+        }
+        return dp[idx] =false;
+    }
+    
+    bool validPartition(vector<int>& nums) {
+         
+        n= nums.size();
+        vector<int>dp(n,-1);
+        return solve(0,nums,dp);
+    }
+};
