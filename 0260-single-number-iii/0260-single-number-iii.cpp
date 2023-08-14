b5e085bd88b97dfb839e88161f7911e373b1d65f
Rahul Maurya <113296853+Maurya2811@users.noreply.github.com>
2023-08-14 22:00:50 +0530

    Time: 7 ms (85.78%), Space: 10.3 MB (36.27%) - LeetHub

--- 0260-single-number-iii/0260-single-number-iii.cpp
+++ 0260-single-number-iii/0260-single-number-iii.cpp
@@ -7,13 +7,14 @@ public:
        for(int &x:nums){
            xy^=x;
        }
-       // xy= (~xy - 1)+ xy ;  // Used to find the first set bit or the first different bit between the 
-       // two nos
-        long long int lastBit =(xy & (xy - 1)) ^ xy;
+       xy= (~xy + 1)& xy ;  
+    // Used to find the first set bit or the first different bit between the two nos
+     
+        // long long int lastBit =(xy & (xy - 1)) ^ xy;
        
        vector<int> vec ={0,0};
        for(int &x:nums){
-           if(lastBit&x)
+           if(xy&x)
                vec[0]^=x;
            else
               vec[1]^=x;
