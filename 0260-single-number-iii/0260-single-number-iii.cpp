3f6270cfd74499d88dfa6a5f9301d15d9fc14ac7
Rahul Maurya <113296853+Maurya2811@users.noreply.github.com>
2023-08-14 21:59:10 +0530

    Time: 6 ms (88.36%), Space: 10.2 MB (37.47%) - LeetHub

---
+++ 0260-single-number-iii/0260-single-number-iii.cpp
@@ -0,0 +1,24 @@
+class Solution
+{
+public:
+    vector<int> singleNumber(vector<int> nums) 
+    {
+       long long int xy =0; // xy will contain the xor of the two unique number
+       for(int &x:nums){
+           xy^=x;
+       }
+       // xy= (~xy - 1)+ xy ;  // Used to find the first set bit or the first different bit between the 
+       // two nos
+        long long int lastBit =(xy & (xy - 1)) ^ xy;
+       
+       vector<int> vec ={0,0};
+       for(int &x:nums){
+           if(lastBit&x)
+               vec[0]^=x;
+           else
+              vec[1]^=x;
+       }
+       
+       return vec;
+    }
+};
