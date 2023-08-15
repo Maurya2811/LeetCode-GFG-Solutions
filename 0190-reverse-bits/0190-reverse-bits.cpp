ad49ebdb305a3730a704cfc397d7c25770d830fe
Rahul Maurya <113296853+Maurya2811@users.noreply.github.com>
2023-08-15 23:28:34 +0530

    Time: 0 ms (100.00%), Space: 6.2 MB (5.70%) - LeetHub

--- 0190-reverse-bits/0190-reverse-bits.cpp
+++ 0190-reverse-bits/0190-reverse-bits.cpp
@@ -4,24 +4,13 @@ public:
         uint32_t rev =0;
        int j=31;
        for(int i=0;i<32;i++){
-          int mask = 1<<i;
            
-           if(n&mask){
-               int smask = 1<< j;
-               rev|=smask;
-           }
+           uint32_t bit = n & (1<<i) ;
+            if(bit)
+            rev |=  ( 1 <<j);
+          
            j--;
        }
         return rev;
     } 
 };
-// class Solution {
-// public:
-//     uint32_t  reverseBits(uint32_t n) {
-//         uint32_t result= 0;
-//         for(int i=0; i<32; i++)
-//             result = (result<<1) + (n>>i &1);
-        
-//         return result;
-//     }
-// };
