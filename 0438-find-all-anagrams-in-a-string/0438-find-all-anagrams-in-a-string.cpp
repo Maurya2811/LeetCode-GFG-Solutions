82df39eb376feab7f6da4e84bbe4d68e880823f8
Rahul Maurya <113296853+Maurya2811@users.noreply.github.com>
2023-08-17 00:59:36 +0530

    Time: 7 ms (96.62%), Space: 8.6 MB (84.67%) - LeetHub

--- 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.cpp
+++ 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.cpp
@@ -1,3 +1,5 @@
+// Using Map
+/*
 class Solution {
 public:
     vector<int> findAnagrams(string s, string p) {
@@ -34,8 +36,51 @@ public:
                 i++;
             }
             j++;
-        }      
+        }            
+            return ans;
+    }
+};
+*/
+
+// Using Constant Space
+
+class Solution {
+public:
+    vector<int> findAnagrams(string s, string p) {
+        vector<int> ans;
+        int k = p.length();     
+        int n = s.length();
+        int count=0;
+        // unordered_map<char,int> mp;
+        vector<int> freq(26,0);
+        for(char &c: p){
+            if(freq[c-'a']==0){  
+                count++;    //  count  = mp.size();
+            }
+            freq[c-'a']++;
+        }
+       
+        
+        int i=0,j=0;
+        while(j<n){
+                freq[s[j]-'a']--;
+                if(freq[s[j]-'a']==0){
+                    count--;
+                }
             
+            if(j-i+1==k){
+                if(count==0){
+                    ans.push_back(i);
+                }
+                
+                freq[s[i]-'a']++;
+                if(freq[s[i]-'a']==1){
+                    count++;
+                }
+                i++;
+              }
+               j++;  
+            }         
             return ans;
     }
 };
