f5de8fd8ee0232a79c0d245db1248c9c96a7f7fd
Rahul Maurya <113296853+Maurya2811@users.noreply.github.com>
2023-08-16 16:40:32 +0530

    Added solution - LeetHub

---
+++ First negative integer in every window of size k - GFG/first-negative-integer-in-every-window-of-size-k.cpp
@@ -0,0 +1,61 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+vector<long long> printFirstNegativeInteger(long long int arr[],
+                                             long long int n, long long int k);
+
+// Driver program to test above functions
+int main() {
+    long long int t, i;
+    cin >> t;
+    while (t--) {
+        long long int n;
+        cin >> n;
+        long long int arr[n];
+        for (i = 0; i < n; i++) {
+            cin >> arr[i];
+        }
+        long long int k;
+        cin >> k;
+
+        vector<long long> ans = printFirstNegativeInteger(arr, n, k);
+        for (auto it : ans) cout << it << " ";
+        cout << endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
+
+
+vector<long long> printFirstNegativeInteger(long long int arr[],
+                                             long long int N, long long int k) {
+                                
+                            long long int i=0,j=0  ;
+                            vector<long long> ans;
+                            vector<long long> temp;
+                        
+                            while(j<N){
+                                if(arr[j]<0)
+                                     temp.push_back(arr[j]);
+                                     
+                                if(j-i+1 == k){
+                                    
+                                    if(temp.size()==0)
+                                       ans.push_back(0);
+                                       
+                                    else {
+                                        int val = temp.front();
+                                        ans.push_back(val);
+                                        if(arr[i]==val){
+                                            temp.erase(temp.begin(),temp.begin()+1);
+                                        }
+                                    } 
+                                    i++;
+                                }
+                                j++;
+                                
+                            }
+                            return ans;
+ }
