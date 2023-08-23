dc8fd9a9539043b8f1089be40da3c23e2a552fe1
Rahul Maurya <113296853+Maurya2811@users.noreply.github.com>
2023-08-23 19:35:28 +0530

    Time: 2 ms (56.28%), Space: 6.1 MB (76.79%) - LeetHub

--- 0767-reorganize-string/0767-reorganize-string.cpp
+++ 0767-reorganize-string/0767-reorganize-string.cpp
@@ -1,4 +1,6 @@
-// Using Max Heap
+//Appraoch 1
+//Using Max Heap
+/*
 class Solution {
 public:
     typedef pair<int,char> P;
@@ -49,3 +51,53 @@ public:
         return ans;
     }
 };
+*/
+
+// Approach 2
+class Solution {
+public:
+    
+    string reorganizeString(string s) {
+        int n = s.length();
+        if(n==1){
+            return s;
+        }
+        vector<int> arr(26,0);
+        int maxCharFreq =0;
+        char maxChar;
+        for(char &ch: s){
+            arr[ch-'a']++;
+            if(arr[ch-'a'] > (n+1)>>1){
+                return "";
+            }
+            if(arr[ch-'a']>maxCharFreq){
+                maxCharFreq = arr[ch-'a'];
+                maxChar = ch;
+            }
+            
+        }
+      
+       
+         int i =0;
+        
+        while(arr[maxChar-'a']){
+            s[i]=maxChar;
+            i+=2;
+            arr[maxChar-'a']--;    
+        }
+        
+         for( int j=0;j<26;j++){
+            while(arr[j]>0){
+                if(i>=n){
+                    i=1;
+                }
+                s[i]= j+'a';
+                i+=2;
+                arr[j]--;
+            }
+        }
+       
+        return s;
+    }
+};
+
