050235f9b1eb747ffb25d7c6537461b0bcd6a3ee
Rahul Maurya <113296853+Maurya2811@users.noreply.github.com>
2023-08-28 19:36:10 +0530

    Time: 4 ms (43.02%), Space: 7.1 MB (62.94%) - LeetHub

--- 0078-subsets/0078-subsets.cpp
+++ 0078-subsets/0078-subsets.cpp
@@ -1,3 +1,5 @@
+// Recursive Approach
+/*
 class Solution {
 public:
      // Given that in All the elements are unique so the Power set will also 
@@ -30,3 +32,30 @@ public:
         return ans;
     }
 };
+*/
+
+//Iterative Approach
+// /*
+class Solution {
+public:
+    vector<vector<int>> subsets(vector<int>& nums) {
+        vector<vector<int>> ans;
+        int n = nums.size();
+        
+        ans.push_back({});
+        
+        for(int i=0; i<n; i++)
+        {
+            int sz = ans.size();
+            for(int j=0; j<sz; j++)
+            {
+                vector<int> temp = ans[j];
+                temp.push_back(nums[i]);
+                ans.push_back(temp);
+            }
+        }
+        
+        return ans;
+    }
+};
+// */
