db0d8af9c3e485c9f3f9310c319f47d7c077085e
Rahul Maurya <113296853+Maurya2811@users.noreply.github.com>
2023-08-30 18:50:13 +0530

    Time: 95 ms (63.66%), Space: 54.7 MB (7.81%) - LeetHub

---
+++ 2366-minimum-replacements-to-sort-the-array/2366-minimum-replacements-to-sort-the-array.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    long long minimumReplacement(vector<int>& nums) {
+        long long operations =0;
+        int n= nums.size();
+        for(int i=n-2;i>=0;i--){
+            
+            if(nums[i]>nums[i+1]){
+                
+                int parts = nums[i]/nums[i+1];
+                  
+                if(nums[i]%nums[i+1]!=0){
+                    parts++;
+                }
+                
+                operations+=parts-1;
+                
+                nums[i]=nums[i]/parts;
+                // Updating the array
+            }
+        }
+        return operations;
+    }
+};
