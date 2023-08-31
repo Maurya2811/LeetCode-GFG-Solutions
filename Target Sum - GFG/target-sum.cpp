d807999107e5ebd3737d049655bf2b5d2c0610f2
Rahul Maurya <113296853+Maurya2811@users.noreply.github.com>
2023-08-31 06:12:40 +0530

    Added solution - LeetHub

---
+++ Target Sum - GFG/target-sum.cpp
@@ -0,0 +1,85 @@
+//{ Driver Code Starts
+//Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+//Back-end complete function Template for C++
+
+class Solution {
+  public:
+     
+    // This problem is same as the
+    // Count the no of the subset with a given diff
+    
+  
+   int countOfSubsetSum(vector<int>& nums, int sum){
+       int n= nums.size();
+       
+       vector<vector<int>> t(n+1,vector<int>(sum+1));
+       
+       for(int j=0;j<sum+1;j++){
+           t[0][j]=0;
+       }
+       for(int i=0;i<n+1;i++){
+           t[i][0]=1;
+       }
+       
+       // t[n][sum] = t[i][j]
+       //  n->i
+       // sum->j
+       for(int i=1;i<n+1;i++){
+           for(int j=0;j<sum+1;j++){
+               if(nums[i-1]<= j){
+                   t[i][j] = t[i-1][j-nums[i-1]] + t[i-1][j];
+               }
+               else              
+                   t[i][j] =  t[i-1][j];
+
+           }
+       }
+       return t[n][sum];
+   }
+    int findTargetSumWays(vector<int>&nums ,int target) {
+        
+       int TotalSum = accumulate(nums.begin(),nums.end(),0);   
+       
+        if((TotalSum+ target)&1 || (TotalSum+target)<0 )
+            return 0;
+        
+        int sum = (TotalSum+ target)>>1;
+        
+            if(sum < target ) return 0;
+            
+        
+        return countOfSubsetSum(nums,sum);
+        
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int N;
+        
+        cin>>N;
+        vector<int>arr(N);
+        
+        for(int i=0 ; i<N ; i++){
+            cin>>arr[i];
+        }
+        int target;
+        cin >> target;
+
+        Solution ob;
+        cout<<ob.findTargetSumWays(arr,target);
+        cout<<"\n";
+    }
+    return 0;
+}
+// } Driver Code Ends
