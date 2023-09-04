739622a10a8b79a38c06a76f1d0e209114e5d4aa
Rahul Maurya <113296853+Maurya2811@users.noreply.github.com>
2023-09-05 01:04:03 +0530

    Time: 4 ms (85.60%), Space: 6.1 MB (8.47%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        if(x<0){
+            return false;
+        }
+        
+        int num1=x;
+        long long num2=0;
+        
+        while(num1){
+            num2 = num2*10 + num1%10 ;
+            num1/=10;
+        }
+        
+        return num2==x;
+    }
+};
