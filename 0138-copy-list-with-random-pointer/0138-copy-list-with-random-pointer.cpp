4f33506e0779643058b104a11ac3642fdb38ae90
Rahul Maurya <113296853+Maurya2811@users.noreply.github.com>
2023-09-06 01:53:49 +0530

    Time: 8 ms (46.68%), Space: 11.4 MB (15.63%) - LeetHub

--- 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.cpp
+++ 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.cpp
@@ -1,73 +1,85 @@
-/*
-// Definition for a Node.
-class Node {
-public:
-    int val;
-    Node* next;
-    Node* random;
-    
-    Node(int _val) {
-        val = _val;
-        next = NULL;
-        random = NULL;
-    }
-};
-*/
-
 class Solution {
 public:
     Node* copyRandomList(Node* head) {
-        if(!head){
-            return head;
-        }
-        Node* tempHead = head;
-        
-      while(tempHead){
-          Node* fwd = tempHead->next;
-          tempHead->next = new Node(tempHead->val);
-          tempHead->next->next = fwd;
-          
-          tempHead = fwd;
-      }
-        
-        // Handling Random
-        tempHead = head;
-       
-
-        
-        while(tempHead){
-            if(tempHead->random)
-            tempHead->next->random = tempHead->random->next;
-            
-            if(tempHead->next){
-                tempHead = tempHead->next->next;
+        if(!head)
+            return NULL;
+        map<Node*, Node*> mp;
+        Node* curr = head;
+        Node* prev = NULL;
+        Node* newHead = NULL;
+        while(curr) {
+            Node* temp = new Node(curr->val);
+            mp[curr] = temp;
+            if(newHead == NULL) {
+                newHead = temp;
+                prev = newHead;
+            } else {
+                prev->next = temp;
+                prev = temp;
             }
-            
+            curr = curr->next;
         }
         
-         Node* currHead = head->next;
-        Node* currNode = currHead;
-        
-         tempHead = head;
-        
-        while(tempHead){
-            
-            if(tempHead->next){
-                tempHead->next=tempHead->next->next;
+        curr = head;
+        Node* newCurr = newHead;
+        while(curr) {
+            if(curr->random == NULL) {
+                newCurr->random = NULL;
+            } else {
+                newCurr->random = mp[curr->random];
             }
             
-            if(currNode->next){
-                currNode->next = currNode->next->next;
-            }
-            
-            
-            tempHead = tempHead->next;
-            currNode = currNode->next;
-            
+            newCurr = newCurr->next;
+            curr = curr->next;
         }
-        
-        
-        
-        return currHead;
+        return newHead;
     }
 };
+
+
+//Approach-2 (O(1) EXTRA space). We can also call it O(n) space for result linked list
+
+/*
+class Solution {
+public:
+    Node* copyRandomList(Node* head) {
+        if(!head)
+            return NULL;
+        
+        Node* curr = head;
+        
+        while(curr) {
+            Node* currNext = curr->next;
+            curr->next = new Node(curr->val);
+            curr->next->next = currNext;
+            curr = currNext;
+        }
+        
+        //Deep copy of random pointers
+        curr = head;
+        while(curr && curr->next) {
+            if(curr->random == NULL) {
+                curr->next->random = NULL;
+            } else {
+                curr->next->random = curr->random->next;
+            }
+            curr = curr->next->next;
+        }
+        
+        //Deep copy of next pointers and recovering old linked list
+        Node* newHead = head->next;
+        Node* newCurr = newHead;
+        curr          = head;
+        while(curr && newCurr) {
+            curr->next    = curr->next ? curr->next->next : NULL;
+            
+            newCurr->next = newCurr->next ? newCurr->next->next : NULL;
+            
+            curr = curr->next;
+            newCurr = newCurr->next;
+        }
+       
+        return newHead;        
+    }
+};
+*/
