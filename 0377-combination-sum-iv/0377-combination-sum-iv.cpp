0cb779ea205f59971a9e940f465a83b98b7075a9
Rahul Maurya <113296853+Maurya2811@users.noreply.github.com>
2023-09-09 16:08:41 +0530

    Time: 0 ms (100.00%), Space: 7.5 MB (6.68%) - LeetHub

--- 0377-combination-sum-iv/0377-combination-sum-iv.cpp
+++ 0377-combination-sum-iv/0377-combination-sum-iv.cpp
@@ -28,19 +28,47 @@ public:
 };
 */
     
+// class Solution {
+// public:
+//     int combinationSum4(vector<int>& nums, int target) {
+//         //
+//             vector<uint>dp(target+1,0);
+        
+//          dp[0]=1;    
+//         for(int i=1;i<=target;i++){
+//             for(int j=0;j<nums.size();j++){
+//                 if(i>=nums[j])
+//                 dp[i] += dp[i-nums[j]];
+//             }
+//         }
+//         return dp[target];
+//     }
+// };
+
 class Solution {
 public:
+    
+    int dp[201][1001];
+    int solve(int idx,vector<int> &nums,int target){
+        
+        if(target ==0)
+            return 1;
+        if(target<0)
+            return 0;
+        if(idx>= nums.size())
+            return 0;
+        
+        if(dp[idx][target]!=-1)return dp[idx][target];
+        
+        // int not_take = solve(idx+1,nums,target);
+        
+        // int take  = solve(0,nums,target-nums[idx]);
+        
+        return dp[idx][target] = (solve(idx+1,nums,target) + solve(0,nums,target-nums[idx]));
+    }
     int combinationSum4(vector<int>& nums, int target) {
         //
-            vector<uint>dp(target+1,0);
-        
-         dp[0]=1;    
-        for(int i=1;i<=target;i++){
-            for(int j=0;j<nums.size();j++){
-                if(i>=nums[j])
-                dp[i] += dp[i-nums[j]];
-            }
-        }
-        return dp[target];
+        memset(dp,-1,sizeof(dp));
+           return solve(0,nums,target);
     }
 };
