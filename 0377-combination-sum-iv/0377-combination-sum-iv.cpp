b3a7a556b14cd65ba61c91cac56c90263bf5b1e7
Rahul Maurya <113296853+Maurya2811@users.noreply.github.com>
2023-09-09 10:53:19 +0530

    Time: 4 ms (28.06%), Space: 6.8 MB (20.91%) - LeetHub

--- 0377-combination-sum-iv/0377-combination-sum-iv.cpp
+++ 0377-combination-sum-iv/0377-combination-sum-iv.cpp
@@ -1,15 +1,13 @@
+// Recursion + Memoization
+/*
 class Solution {
 public:
     
-    // int ans =0;
-    int n;
     int dp[1001];
-    
     int solve(vector<int>& nums, int target){
-    
-         if(target==0){
+         if(target==0)
            return 1;
-        }
+      
       if(dp[target]!=-1)
           return dp[target];
         
@@ -18,21 +16,31 @@ public:
           if(target>=x)
          ans += solve(nums,target-x);
       } 
-         dp[target]=ans;
-//         int ans1 = solve(nums,idx+1,sum,target) ;
-//         int ans2= solve(nums,idx,sum+nums[idx],target);
-//         int ans3 =solve(nums,idx+1,sum+nums[idx],target);
-        
-        // return ans1+ans2+ ans3;
+          dp[target]=ans;
         return dp[target];
     }
     
     int combinationSum4(vector<int>& nums, int target) {
-        
         memset(dp,-1,sizeof(dp));
-      
-        n= nums.size();
        return solve(nums,target);
   
+    }
+};
+*/
+    
+class Solution {
+public:
+    int combinationSum4(vector<int>& nums, int target) {
+        //
+            vector<uint>dp(target+1,0);
+        
+         dp[0]=1;    
+        for(int i=1;i<=target;i++){
+            for(int j=0;j<nums.size();j++){
+                if(i>=nums[j])
+                dp[i] += dp[i-nums[j]];
+            }
+        }
+        return dp[target];
     }
 };
