3b2ebc7d996f818040d717d99413e08fca48e4c1
Rahul Maurya <113296853+Maurya2811@users.noreply.github.com>
2023-09-14 04:30:43 +0530

    Time: 2 ms (43.85%), Space: 6.3 MB (14.39%) - LeetHub

--- 1492-the-kth-factor-of-n/1492-the-kth-factor-of-n.cpp
+++ 1492-the-kth-factor-of-n/1492-the-kth-factor-of-n.cpp
@@ -3,13 +3,28 @@ public:
     int kthFactor(int n, int k) {
         
         
-        for(int i=1;i<=n;i++){
-            if(n%i==0){
-                k--;
-                if(k==0)
-                    return i;
-            }
-        }
-        return -1;
+        // for(int i=1;i<=n;i++){
+        //     if(n%i==0){
+        //         k--;
+        //         if(k==0)
+        //             return i;
+        //     }
+        // }
+        // return -1;
+        
+        
+         for (int i = 1; i < sqrt(n); ++i)
+	            if (n % i == 0 && --k == 0)
+		                return i;
+        
+        
+         for (int i = (int)sqrt(n); i >= 1; --i)
+	            if (n % (n / i) == 0 && --k == 0)
+		                return n / i;
+        
+        
+           return -1;
+        
     }
 };
+
