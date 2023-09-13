a66900246e64503e13112b547d487cacf4ca9f59
Rahul Maurya <113296853+Maurya2811@users.noreply.github.com>
2023-09-13 15:13:44 +0530

    Time: 17 ms (62.30%), Space: 18.2 MB (15.54%) - LeetHub

---
+++ 0135-candy/0135-candy.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int candy(vector<int>& ratings) {
+        
+        int n = ratings.size();
+        if(n==1)
+            return 1;
+        
+        
+        vector<int> temp(n,1);
+       
+        for(int i=1;i<n;i++){
+            if(ratings[i]>ratings[i-1]){
+                temp[i]=max(temp[i],temp[i-1]+1);
+            }
+        }
+        for(int i=n-2;i>=0;i--){
+            if(ratings[i]>ratings[i+1]){
+                temp[i]=max(temp[i],temp[i+1]+1);
+            }
+        }
+        
+        return accumulate(temp.begin(),temp.end(),0);
+        
+    }
+};
