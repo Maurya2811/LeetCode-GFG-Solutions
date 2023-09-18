896d28d33a1fbf2b757b34ac8ce8aca210458b00
Rahul Maurya <113296853+Maurya2811@users.noreply.github.com>
2023-09-18 19:09:01 +0530

    Time: 6 ms (91.97%), Space: 11.4 MB (9.15%) - LeetHub

--- 1337-the-k-weakest-rows-in-a-matrix/1337-the-k-weakest-rows-in-a-matrix.cpp
+++ 1337-the-k-weakest-rows-in-a-matrix/1337-the-k-weakest-rows-in-a-matrix.cpp
@@ -1,17 +1,40 @@
 class Solution {
 public:
+    
+    int col;
+    
+    int countNoOfOne(vector<int>arr){
+        int s=0,e=col-1;
+        int ans=-1;
+        while(s<=e){
+            int mid = (s+e)/2;
+                        // cout<< mid<<" ";
+            if(arr[mid]==1){
+                ans=mid;
+                s=mid+1;
+            }
+            else{
+                e=mid-1;
+            }
+        }
+        return ans+1;
+    }
     vector<int> kWeakestRows(vector<vector<int>>& mat, int k) {
         
         int row = mat.size();
+        col = mat[0].size();
         vector<pair<int,int>> vec;
         
         for(int j=0; j < row ;j++){
-            int countOne=0;
-            for(int i=0;i<mat[j].size();i++){
-                if(mat[j][i]==1){
-                    countOne++;
-                }
-            }
+            int countOne= countNoOfOne(mat[j]);
+            
+            // Linear Search
+            
+            // for(int i=0;i<col;i++){
+            //     if(mat[j][i]==1){
+            //         countOne++;
+            //     }
+            // }
             vec.push_back({countOne,j});
         }
         sort(vec.begin(),vec.end());
