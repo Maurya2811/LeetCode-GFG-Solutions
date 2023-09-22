27370fbb6641a1b9b2f80bb0d3fab2d156c7658e
Rahul Maurya <113296853+Maurya2811@users.noreply.github.com>
2023-09-22 09:00:53 +0530

    Time: 4 ms (10.63%), Space: 9.3 MB (5.02%) - LeetHub

--- 0392-is-subsequence/0392-is-subsequence.cpp
+++ 0392-is-subsequence/0392-is-subsequence.cpp
@@ -2,31 +2,36 @@ class Solution {
 public:
     int m,n;
     
-    bool solve(int i, int j, string &s, string &t){
+    bool solve(int i, int j, string &s, string &t,vector<vector<int>> &dp){
         if(i>=m)
             return true;
         if(j>=n)
             return false;
         
+        if(dp[i][j]!=-1)
+            return dp[i][j];
+        
         bool ans1 = false;
         bool ans2 = false;
+        
+        
         if(s[i]==t[j]){
-            ans1 = solve(i+1,j+1,s,t);
+            ans1 = solve(i+1,j+1,s,t,dp);
             if(ans1)
-                return true;
+                return dp[i][j] = true;
         }
           
         else{
-          ans2 = solve(i,j+1,s,t);
+          ans2 = solve(i,j+1,s,t,dp);
         }
         
-        return ans1 | ans2 ;
+        return dp[i][j] = (ans1 | ans2) ;
     }
     
     bool isSubsequence(string s, string t) {
         m=s.length();
         n=t.length();
-        
-        return solve(0,0,s,t);
+        vector<vector<int>> dp(m+1,vector<int>(n+1,-1));
+        return solve(0,0,s,t,dp);
     }
 };
