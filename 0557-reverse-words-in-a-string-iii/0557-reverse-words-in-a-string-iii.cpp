f72e4840f3ccafd54b3cfc89636c42964e131df5
Rahul Maurya <113296853+Maurya2811@users.noreply.github.com>
2023-10-01 15:49:57 +0530

    Time: 17 ms (46.37%), Space: 10 MB (59.58%) - LeetHub

--- 0557-reverse-words-in-a-string-iii/0557-reverse-words-in-a-string-iii.cpp
+++ 0557-reverse-words-in-a-string-iii/0557-reverse-words-in-a-string-iii.cpp
@@ -1,20 +1,19 @@
 class Solution {
 public:
     string reverseWords(string s) {
-            string temp=" ";
-            string ans="";
-        int n = s.length();
-            for(int i=0;i<n;i++){
-                if(s[i]==' '){
-                    ans+=temp;
-                    temp=" ";
-                    i++;
-                }
-                temp=s[i]+temp;
-            }
-        ans+=temp;
-        ans.pop_back();
         
-        return ans;
+        int n= s.length();
+          
+          for(int i=0;i<n;i++){
+
+              int j=i;
+              while(j<n && s[j]!=' '){
+                  j++;
+              }
+       
+              reverse(s.begin()+i , s.begin()+j);
+              i=j;
+          }
+        return s;
     }
 };
