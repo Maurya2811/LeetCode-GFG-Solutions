801e9a25ae8e16a4fc44af767e57b5a0be0cc0db
Rahul Maurya <113296853+Maurya2811@users.noreply.github.com>
2023-10-02 13:25:30 +0530

    Time: 25 ms (97.2%), Space: 13.6 MB (17.42%) - LeetHub

--- 2149-remove-colored-pieces-if-both-neighbors-are-the-same-color/2149-remove-colored-pieces-if-both-neighbors-are-the-same-color.cpp
+++ 2149-remove-colored-pieces-if-both-neighbors-are-the-same-color/2149-remove-colored-pieces-if-both-neighbors-are-the-same-color.cpp
@@ -8,18 +8,27 @@ public:
               return false;
           }
         for(int i=0;i<n-2;i++){
-            if(colors[i]=='A'){
-              if(colors[i+1]==colors[i+2] && colors[i+1]=='A'){
-                alice_count++;
+            
+              if(colors[i]==colors[i+1] && colors[i+1]==colors[i+2]){
+                if(colors[i]=='A')
+                   alice_count++;
+                else
+                   bob_count++;
               }
             }
-            else if(colors[i]=='B'){
-                 if( colors[i+1]==colors[i+2]&&colors[i+1]=='B'){
-                  bob_count++;
-               }
-            }
-
-        }
+            
+            // if(colors[i]=='A'){
+            //   if(colors[i+1]==colors[i+2] && colors[i+1]=='A'){
+            //     alice_count++;
+            //   }
+            // }
+            // else if(colors[i]=='B'){
+            //      if( colors[i+1]==colors[i+2]&&colors[i+1]=='B'){
+            //       bob_count++;
+            //    }
+            // }
+            // }
+        
         return alice_count>bob_count;
     }
 };
