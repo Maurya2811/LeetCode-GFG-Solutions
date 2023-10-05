90df18ac513adc6a8d5eaef9a05b974562e1cfa8
Rahul Maurya <113296853+Maurya2811@users.noreply.github.com>
2023-10-05 22:29:24 +0530

    Time: 7 ms (87.9%), Space: 16.1 MB (36.8%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.cpp
+++ 0229-majority-element-ii/0229-majority-element-ii.cpp
@@ -1,35 +1,54 @@
 class Solution {
 public:
     vector<int> majorityElement(vector<int>& nums) {
-        int n= nums.size();
-        if(n==1)
-        return nums;
+        
+        int count1=0;
+        int maj1=NULL;
+        int count2=0;
+        int maj2=NULL;
 
-        if(n==2){
-            if(nums[0]==nums[1]){
-                return {nums[0]};
-            }
-            return nums;
+        int n = nums.size();
+
+        for(int i=0;i<n;i++){
+
+              if(nums[i]==maj1){
+                  count1++;
+              }
+             else if(nums[i]==maj2){
+                  count2++;
+              }
+             else if(count1==0){
+                maj1=nums[i];
+                count1=1;
+             }
+             else if(count2==0){
+                maj2=nums[i];
+                count2=1;
+             }
+             else{
+                 count1--;
+                 count2--;
+             }
         }
 
-        set<int> st;
-        int size = n/3;
-         vector<int> ans;
-         for(int i=0;i<n-1;i++){
-             int freq =1;
-             int ele = nums[i];
-             for(int j=i+1;j<n;j++){
-                 if(nums[j]==ele){
-                     freq++;          
-                     }
-                 if(freq>size){    
-                          st.insert(nums[i]);
-                          break;
-                      }
-                 }
-             }
-          
+          int freq1=0,freq2=0;
+        for(int &x:nums){ 
+            if(x==maj1){
+                freq1++;
+            }
+           else if(x==maj2){
+                freq2++;
+            }
+        }
 
-         return vector<int>(st.begin(),st.end());
-         }
+ vector<int> ans;
+        if(freq1>(n/3)){
+              ans.push_back(maj1);
+        }
+        if(freq2>(n/3)){
+              ans.push_back(maj2);
+        }
+     
+     return ans;
+    }
 };
