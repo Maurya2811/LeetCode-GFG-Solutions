cc78fa44c1bb785324664c68e885be4c7f101030
Rahul Maurya <113296853+Maurya2811@users.noreply.github.com>
2023-10-20 00:55:12 +0530

    Time: 0 ms (100%), Space: 6.8 MB (15.59%) - LeetHub

--- 0874-backspace-string-compare/0874-backspace-string-compare.cpp
+++ 0874-backspace-string-compare/0874-backspace-string-compare.cpp
@@ -1,6 +1,55 @@
 class Solution {
 public:
     bool backspaceCompare(string s, string t) {
+        int skip_s=0;
+        int skip_t=0;
+        int i=s.length()-1;
+        int j=t.length()-1;
+       while(i >= 0 || j >= 0) {
+            
+            while(i >= 0) {
+                if(s[i] == '#') {
+                    skip_s++;
+                    i--;
+                } else if(skip_s > 0) {
+                    skip_s--;
+                    i--;
+                } else {
+                    break;
+                }
+            }
+            
+            while(j >= 0) {
+                if(t[j] == '#') {
+                    skip_t++;
+                    j--;
+                } else if(skip_t > 0) {
+                    skip_t--;
+                    j--;
+                } else {
+                    break;
+                }
+            }
+            
+            char first  = i < 0 ? '$' : s[i];
+            char second = j < 0 ? '$' : t[j];
+            
+            if(first != second)
+                return false;
+            
+            i--;
+            j--;
+        }
+        
+        return true;
+    }
+};
+
+// Brute Force
+/*
+class Solution {
+public:
+    bool backspaceCompare(string s, string t) {
         
         stack<int>s1;
         for(char &ch:s){
@@ -40,3 +89,4 @@ public:
 
     }
 };
+*/
